df9dc3a34565456d1b9bb4a929a261d076c5a4c9
Minh Nguyen <minhnguyenxx@gmail.com>
2020-05-26 11:51:24 +0100

    Refactor ESLint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,8 @@ module.exports = {
   },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
     'prettier',
     'prettier/react',
   ],
@@ -17,33 +16,39 @@ module.exports = {
     Atomics: 'readonly',
     SharedArrayBuffer: 'readonly',
   },
-  parser: '@typescript-eslint/parser',
+  parser: 'babel-eslint',
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
     ecmaVersion: 2020,
-    // See: https://github.com/typescript-eslint/typescript-eslint/issues/1928#issuecomment-617969784
-    project: ['./tsconfig.eslint.json'],
     sourceType: 'module',
-    // `.eslintrc.js` needs to be a JavaScript file to access `__dirname` since we want linting to
-    // work if we run it from a different current working directory.
-    // See: https://github.com/typescript-eslint/typescript-eslint/issues/101#issuecomment-499303058
-    //
-    // However, that gives an error message because `__dirname` is defined within `@types/node` which
-    // we do not currently load. Therefore, we disable the following rule because the project is a
-    // mixture of browser-based and Node-based code, and the solutions to this are either overkill
-    // or unelegant (e.g. solution-style `tsconfig.json`s, or for linting only using overrides to enable
-    // TypeScript eslint rules only on TypeScript files).
-    // TODO: https://github.com/jpmorganchase/modular/issues/9
-    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-    tsconfigRootDir: __dirname,
   },
-  plugins: ['@typescript-eslint', 'react'],
+  plugins: ['react', 'react-hooks'],
   rules: {},
   settings: {
     react: {
       version: 'detect',
     },
   },
+  overrides: [
+    {
+      files: ['*.ts?(x)'],
+      extends: [
+        'plugin:@typescript-eslint/recommended',
+        'plugin:@typescript-eslint/recommended-requiring-type-checking',
+        'prettier/@typescript-eslint',
+      ],
+      parserOptions: {
+        // See: https://github.com/typescript-eslint/typescript-eslint/issues/1928#issuecomment-617969784
+        project: ['./tsconfig.json'],
+        // `.eslintrc.js` needs to be a JavaScript file to access `__dirname` since we want linting to
+        // work if we run it from a different current working directory.
+        // See: https://github.com/typescript-eslint/typescript-eslint/issues/101#issuecomment-499303058
+        // TODO: https://github.com/jpmorganchase/modular/issues/9
+        tsconfigRootDir: __dirname,
+      },
+      plugins: ['@typescript-eslint'],
+    },
+  ],
 };
