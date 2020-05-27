b8693e9b775afb648c46f49f3e6f0a75e0922d50
Minh Nguyen <minhnguyenxx@gmail.com>
2020-05-27 11:21:46 +0100

    Fix compilation error
    This is a follow-up to #13.

--- packages/cli/cli.ts
+++ packages/cli/cli.ts
@@ -1,1 +1,4 @@
 // not clear yet, how to test the parts. this is just a starter file.
+// Fix TS1208: All files must be modules when the '--isolatedModules' flag is provided.
+// https://stackoverflow.com/a/56577324
+export {};
