27b20a748714e4a9e7498754eb9d7f17b911b3c7
Seb Insua <me@sebinsua.com>
2020-07-13 15:39:26 +0100

    Import with param-case package name instead of PascalCase component name (#28)

--- packages/cli/template/widgets/starter/index.js
+++ packages/cli/template/widgets/starter/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export default function Component$$() {
-  return <div>This is Component$$</div>;
+export default function ComponentName$$() {
+  return <div>This is ComponentName$$</div>;
 }
