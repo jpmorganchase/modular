1c6ecd3008162b7655fb48e844b1ef4236974c5f
Minh Nguyen <minhnguyenxx@gmail.com>
2020-09-23 18:03:49 +0300

    Convert arrow functions to named functions

--- scripts/listChangedFiles.js
+++ scripts/listChangedFiles.js
@@ -5,7 +5,7 @@
 
 const { execFileSync } = require('child_process');
 
-const exec = (command, args) => {
+function exec(command, args) {
   console.log('> ' + [command].concat(args).join(' '));
   const options = {
     cwd: process.cwd(),
@@ -14,16 +14,16 @@ const exec = (command, args) => {
     encoding: 'utf-8',
   };
   return execFileSync(command, args, options);
-};
+}
 
 const execGitCmd = (args) => exec('git', args).trim().toString().split('\n');
 
-const listChangedFiles = () => {
+function listChangedFiles() {
   const mergeBase = execGitCmd(['merge-base', 'HEAD', 'master']);
   return new Set([
     ...execGitCmd(['diff', '--name-only', '--diff-filter=ACMRTUB', mergeBase]),
     ...execGitCmd(['ls-files', '--others', '--exclude-standard']),
   ]);
-};
+}
 
 module.exports = listChangedFiles;
