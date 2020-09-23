4a6e6694b4e0f78d575ad8ce3abddbcdda31cb0b
Minh Nguyen <minhnguyenxx@gmail.com>
2020-09-23 18:27:57 +0300

    Add linc (#108)
    Co-authored-by: Sunil Pai <threepointone@gmail.com>

---
+++ scripts/listChangedFiles.js
@@ -0,0 +1,31 @@
+// Based on similar script in React
+// https://github.com/facebook/react/blob/b87aabd/scripts/shared/listChangedFiles.js
+
+'use strict';
+
+const { execFileSync } = require('child_process');
+
+function exec(command, args) {
+  console.log('> ' + [command].concat(args).join(' '));
+  const options = {
+    cwd: process.cwd(),
+    env: process.env,
+    stdio: 'pipe',
+    encoding: 'utf-8',
+  };
+  return execFileSync(command, args, options);
+}
+
+function execGitCmd(args) {
+  return exec('git', args).trim().toString().split('\n');
+}
+
+function listChangedFiles() {
+  const mergeBase = execGitCmd(['merge-base', 'HEAD', 'master']);
+  return new Set([
+    ...execGitCmd(['diff', '--name-only', '--diff-filter=ACMRTUB', mergeBase]),
+    ...execGitCmd(['ls-files', '--others', '--exclude-standard']),
+  ]);
+}
+
+module.exports = listChangedFiles;
