54372f68797d5f31a946252c7014752455545399
Sunil Pai <threepointone@gmail.com>
2020-11-26 14:16:20 +0000

    delete cra-template-modular-typescript (#176)
    `cra-template-modular-typescript` is not necessary anymore since https://github.com/jpmorganchase/modular/pull/171/ landed. This PR removes it.
    
    Co-authored-by: Sunil Pai <sunil.pai@jpmorgan.com>

--- packages/modular-scripts/cra-template-modular-typescript/README.md
+++
@@ -1,4 +0,0 @@
-# cra-template-modular-typescript
-
-This is the official TypeScript template for
-[modular](https://github.com/jpmorganchase/modular).
--- packages/modular-scripts/cra-template-modular-typescript/template/src/__tests__/App.test.tsx
+++
@@ -1,9 +0,0 @@
-import * as React from 'react';
-import { render } from '@testing-library/react';
-import App from '../App';
-
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
