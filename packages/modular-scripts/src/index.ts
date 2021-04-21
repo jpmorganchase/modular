36287e6bacd1cf337513fad2cbfb20ec2bfae9f9
Luke Sheard <luke.sheard@jpmorgan.com>
2021-04-21 10:48:36 +0100

    split commands into file based APIs and expose natively

---
+++ packages/modular-scripts/src/index.ts
@@ -0,0 +1,4 @@
+export { default as build } from './build';
+export { default as start } from './start';
+export { default as test } from './test';
+export { default as addPackage } from './addPackage';
