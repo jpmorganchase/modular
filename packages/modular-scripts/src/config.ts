65aaaa64852d53c4f4ba4124a987685d9d5723a5
Luke Sheard <luke.sheard@jpmorgan.com>
2021-04-21 11:31:07 +0100

    correct craco path

--- packages/modular-scripts/src/config.ts
+++ packages/modular-scripts/src/config.ts
@@ -8,6 +8,5 @@ export const cracoBin = resolveAsBin('craco');
 export const cracoConfig = path.join(
   __dirname,
   '..',
-  '..',
   'DO_NOT_IMPORT_THIS_OR_YOU_WILL_BE_FIRED_craco.config.js',
 );
