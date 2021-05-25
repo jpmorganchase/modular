202c1198d4f00b4d256c9d04835e810a3f12b00e
Luke Sheard <LukeSheard@users.noreply.github.com>
2021-05-25 12:55:02 +0100

    memoize calls which are costly and can be called multiple times (#384)
    Co-authored-by: Luke Sheard <luke.sheard@jpmorgan.com>

--- packages/modular-scripts/src/utils/workspaces.ts
+++ packages/modular-scripts/src/utils/workspaces.ts
@@ -1,20 +1,26 @@
 import execa from 'execa';
+import memoize from './memoize';
 import getModularRoot from './getModularRoot';
 
-let workspaces: { [name: string]: { location: string } };
-
-export function getAllWorkspaces(): typeof workspaces {
-  if (!workspaces) {
-    const modularRoot = getModularRoot();
-
-    workspaces = JSON.parse(
-      execa.sync('yarnpkg', ['workspaces', 'info'], {
-        all: true,
-        reject: false,
-        cwd: modularRoot,
-        cleanup: true,
-      }).stdout,
-    ) as { [name: string]: { location: string } };
-  }
-  return workspaces;
+interface YarnWorkspace {
+  location: string;
+  workspaceDependencies: string[];
+  mismatchedWorkspaceDependencies: string[];
 }
+
+type YarnWorkspaces = Record<string, YarnWorkspace>;
+
+function _getAllWorkspaces(): YarnWorkspaces {
+  const modularRoot = getModularRoot();
+
+  return JSON.parse(
+    execa.sync('yarnpkg', ['workspaces', 'info'], {
+      all: true,
+      reject: false,
+      cwd: modularRoot,
+      cleanup: true,
+    }).stdout,
+  ) as YarnWorkspaces;
+}
+
+export const getAllWorkspaces = memoize(_getAllWorkspaces);
