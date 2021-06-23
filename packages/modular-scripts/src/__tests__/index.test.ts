e435d6d6c617e3f288337fe0b23e48c0cd069a05
Cang Truong <cang.truong@chase.com>
2021-06-23 08:36:15 -0400

    snaps and remove types/lockfile

--- packages/modular-scripts/src/__tests__/index.test.ts
+++ packages/modular-scripts/src/__tests__/index.test.ts
@@ -398,11 +398,11 @@ describe('modular-scripts', () => {
       "sample-view
       ├─ README.md #11adaka
       ├─ dist-cjs
-      │  ├─ sample-view.cjs.js #8jw6cg
-      │  └─ sample-view.cjs.js.map #130r3z8
+      │  ├─ sample-view.cjs.js #10vycz1
+      │  └─ sample-view.cjs.js.map #15ks78h
       ├─ dist-es
-      │  ├─ sample-view.es.js #1ctbbz8
-      │  └─ sample-view.es.js.map #12deywy
+      │  ├─ sample-view.es.js #1rtqi2k
+      │  └─ sample-view.es.js.map #1sky7si
       ├─ dist-types
       │  └─ src
       │     └─ index.d.ts #1vloh7q
