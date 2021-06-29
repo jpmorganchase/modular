0fe8a5b55a106848552ed5ddc27f04d23287366e
Luke Sheard <me@lukesheard.com>
2021-06-29 15:23:27 +0100

    remove craco references

--- packages/modular-scripts/react-scripts/config/paths.js
+++ packages/modular-scripts/react-scripts/config/paths.js
@@ -7,7 +7,7 @@ const getPublicUrlOrPath = require('react-dev-utils/getPublicUrlOrPath');
 if (!process.env.MODULAR_ROOT) {
   throw new Error(
     // this should never be visible to a user, only us when we're developing
-    'MODULAR_ROOT not found in environment, did you forget to pass it when calling cracoBin in cli.ts?',
+    'MODULAR_ROOT not found in environment, did you forget to pass it when calling react-scripts in cli.ts?',
   );
 }
 
