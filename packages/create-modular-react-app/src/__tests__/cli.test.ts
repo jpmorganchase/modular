cbb5f67632f81d7dfb77eba524ff69583699e4ff
Luke Sheard <luke.sheard@jpmorgan.com>
2021-06-30 15:02:11 +0100

    fix issues with tests

--- packages/create-modular-react-app/src/__tests__/cli.test.ts
+++ packages/create-modular-react-app/src/__tests__/cli.test.ts
@@ -1,6 +1,6 @@
 import execa from 'execa';
-import * as path from 'path';
 import * as fs from 'fs-extra';
+import * as tmp from 'tmp';
 import tree from 'tree-view-for-tests';
 
 // this can take a while...
@@ -9,11 +9,11 @@ jest.setTimeout(10 * 60 * 1000);
 describe('Creating a new modular app via the CLI', () => {
   let cwd: string;
   beforeEach(async () => {
-    cwd = path.join(__dirname, '..', '..', 'new-modular-app');
+    cwd = tmp.dirSync().name;
 
-    await fs.remove(cwd);
+    console.log('Using ', cwd);
 
-    await execa('yarnpkg', ['create-modular-react-app', 'new-modular-app'], {
+    await execa('yarnpkg', ['create-modular-react-app', cwd], {
       cwd: __dirname,
       cleanup: true,
       stderr: process.stderr,
