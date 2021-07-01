30b370e20625540bfae83364ef72f3c42ced9491
Cang Truong <cang.truong@chase.com>
2021-07-01 07:52:54 -0400

    crawl through dependencies

--- packages/modular-scripts/src/port.ts
+++ packages/modular-scripts/src/port.ts
@@ -1,21 +1,22 @@
 import { JSONSchemaForNPMPackageJsonFiles as PackageJson } from '@schemastore/package';
 import * as fs from 'fs-extra';
 import * as path from 'path';
+import rimraf from 'rimraf';
+import getModularRoot from './utils/getModularRoot'
 import actionPreflightCheck from './utils/actionPreflightCheck';
 import addPackage from './addPackage';
-import rimraf from 'rimraf';
+import { ModularPackageJson } from './utils/isModularType';
 
 export async function port(relativePath: string): Promise<void> {
   try {
-    // const modularRoot = getModularRoot();
     const targetedAppPath = path.resolve(relativePath);
-    console.log('targetedAppPath: ', targetedAppPath);
     const targetedAppPackageJson = (await fs.readJSON(
       path.join(targetedAppPath, 'package.json'),
     )) as PackageJson;
-    const targetedAppName = targetedAppPackageJson.name;
+    const targetedAppName = targetedAppPackageJson.name as string;
     await addPackage(targetedAppName, 'app', targetedAppName);
-    // Replace the template src and public folders with current react app folders
+
+    // Replace the template src and public folders with targeted app folders
     const srcFolders = ['src', 'public'];
     srcFolders.forEach((dir: string) => {
       if (fs.existsSync(path.join(targetedAppPath, dir))) {
@@ -28,6 +29,17 @@ export async function port(relativePath: string): Promise<void> {
         );
       }
     });
+
+    const modularRoot = getModularRoot();
+    const dependencies = ['dependencies', 'devDependencies'];
+    const modularRootPackageJson = (await fs.readJSON(path.join(modularRoot, 'package.json'))) as ModularPackageJson;
+
+    dependencies.forEach((dependency) => {
+      const rootDep = modularRootPackageJson[dependency]
+      const targetedDep = targetedAppPackageJson[dependency]
+      const targetedDepList = Object.keys(targetedDep);
+    })
+
   } catch (err) {
     console.log(err);
   }
