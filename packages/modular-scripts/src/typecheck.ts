10b0a27b621bfe205769e7dd3c32e5194142c48b
Cang Truong <cang.truong@chase.com>
2021-07-15 14:54:17 -0400

    tsc api to print flat for non CI or pretty for CI

--- packages/modular-scripts/src/typecheck.ts
+++ packages/modular-scripts/src/typecheck.ts
@@ -1,13 +1,56 @@
 import isCI from 'is-ci';
-import execa from 'execa';
-import getModularRoot from './utils/getModularRoot';
+import path from 'path';
+import ts from 'typescript';
+import getPackageMetadata from './buildPackage/getPackageMetadata';
+import { reportTSDiagnostics } from './buildPackage/reportTSDiagnostics';
 
 export async function typecheck(): Promise<void> {
-  const options = ['--pretty'];
-  if (isCI) {
-    options.push('--noEmit');
+  const { typescriptConfig } = await getPackageMetadata();
+
+  const { _compilerOptions, ...tsConfig } = typescriptConfig;
+
+  // Parse all config except for compilerOptions
+  const configParseResult = ts.parseJsonConfigFileContent(
+    tsConfig,
+    ts.sys,
+    path.dirname('tsconfig.json'),
+  );
+
+  if (configParseResult.errors.length > 0) {
+    reportTSDiagnostics(':root', configParseResult.errors);
+    throw new Error('Could not parse Typescript configuration');
   }
-  await execa('tsc', options, {
-    cwd: getModularRoot(),
-  });
+
+  const program = ts.createProgram(
+    configParseResult.fileNames,
+    configParseResult.options,
+  );
+
+  // Pulled from typescript's getCanonicalFileName logic
+  // eslint-disable-next-line  no-useless-escape
+  const fileNameLowerCaseRegExp = /[^\u0130\u0131\u00DFa-z0-9\\/:\-_\. ]+/g;
+
+  function toFileNameLowerCase(x: string) {
+    return fileNameLowerCaseRegExp.test(x)
+      ? x.replace(fileNameLowerCaseRegExp, x.toLowerCase())
+      : x;
+  }
+
+  const diagnostics = ts.getPreEmitDiagnostics(program) as ts.Diagnostic[];
+
+  const diagnosticHost = {
+    getCurrentDirectory: (): string => ts.sys.getCurrentDirectory(),
+    getNewLine: (): string => ts.sys.newLine,
+    getCanonicalFileName: (file: string): string =>
+      ts.sys.useCaseSensitiveFileNames ? file : toFileNameLowerCase(file),
+  };
+
+  if (isCI && diagnostics.length) {
+    throw new Error(ts.formatDiagnostics(diagnostics, diagnosticHost));
+  }
+  if (diagnostics.length) {
+    throw new Error(
+      ts.formatDiagnosticsWithColorAndContext(diagnostics, diagnosticHost),
+    );
+  }
 }
