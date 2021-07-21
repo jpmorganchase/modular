aeb00d7aa8b7d2862f4e2d034c8df7c3520ccc1b
Cang Truong <cang.truong@chase.com>
2021-07-21 13:27:29 -0400

    use ternary

--- packages/modular-scripts/jest-runner-eslint/config.js
+++ packages/modular-scripts/jest-runner-eslint/config.js
@@ -3,7 +3,9 @@
 const esLintCLIConfig = {
   cache: true,
   maxWarnings: 0,
-  fix: process.env.MODULAR_LINT_FIX?.toLowerCase() === 'true',
+  fix: process.env.MODULAR_LINT_FIX
+    ? process.env.MODULAR_LINT_FIX.toLowerCase() === 'true'
+    : false,
 };
 
 module.exports = () => esLintCLIConfig;
