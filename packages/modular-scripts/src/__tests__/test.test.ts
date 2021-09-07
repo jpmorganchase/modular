7afdaab875abdaa2c3d39d0cb3d624770ada4e84
joshwooding <12938082+joshwooding@users.noreply.github.com>
2021-09-07 12:03:36 +0100

    Fix `modular test` returning exit code 0 when tests fail

---
+++ packages/modular-scripts/src/__tests__/test.test.ts
@@ -0,0 +1,72 @@
+import execa, { ExecaError } from 'execa';
+import path from 'path';
+import * as fs from 'fs-extra';
+
+const fixturesFolder = path.join(__dirname, '__fixtures__', 'test');
+
+function setupTests() {
+  const files = fs.readdirSync(path.join(fixturesFolder));
+  files.forEach((file) => {
+    fs.writeFileSync(
+      path.join(fixturesFolder, file),
+      fs
+        .readFileSync(path.join(fixturesFolder, file), 'utf-8')
+        .replace('describe.skip', 'describe'),
+    );
+  });
+}
+
+function clearTests() {
+  const files = fs.readdirSync(path.join(fixturesFolder));
+  files.forEach((file) => {
+    fs.writeFileSync(
+      path.join(fixturesFolder, file),
+      fs
+        .readFileSync(path.join(fixturesFolder, file), 'utf-8')
+        .replace('describe', 'describe.skip'),
+    );
+  });
+}
+
+describe('Modular test', () => {
+  beforeEach(() => {
+    setupTests();
+  });
+
+  afterEach(() => {
+    clearTests();
+  });
+  describe('when the tests fail', () => {
+    it('should exit with an error', async () => {
+      let errorNumber = 0;
+      try {
+        await execa(
+          'yarnpkg',
+          ['modular', 'test', 'test/InvalidTest.test.ts'],
+          {
+            all: true,
+            cleanup: true,
+          },
+        );
+      } catch (error) {
+        errorNumber = (error as ExecaError).exitCode;
+      }
+      expect(errorNumber).toEqual(1);
+    });
+  });
+
+  describe('when the tests pass', () => {
+    it('should exit with no error', async () => {
+      let errorNumber = 0;
+      try {
+        await execa('yarnpkg', ['modular', 'test', 'test/ValidTest.test.ts'], {
+          all: true,
+          cleanup: true,
+        });
+      } catch (error) {
+        errorNumber = (error as ExecaError).exitCode;
+      }
+      expect(errorNumber).toEqual(0);
+    });
+  });
+});
