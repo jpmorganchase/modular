477ae04d5d7bf22ec1a5236480fc5a422a7ecf70
Luke Sheard <luke.sheard@jpmorgan.com>
2021-10-14 17:42:56 +0100

    bugfix es2017 syntax in modular with esbuild

---
+++ .changeset/wild-avocados-smile.md
@@ -0,0 +1,6 @@
+---
+'modular-scripts': patch
+---
+
+Fix for defaultbrowser lists to handle syntax which is not supported in webpack
+from esbuild.
--- packages/modular-scripts/react-dev-utils/clearConsole.js
+++ packages/modular-scripts/react-dev-utils/clearConsole.js
@@ -1,9 +1,17 @@
 'use strict';
 
+const isCI = require('is-ci');
+
+const DEBUG =
+  process.env.MODULAR_LOGGER_DEBUG || process.argv.includes('--verbose');
+const isInteractive = process.stdout.isTTY;
+
 function clearConsole() {
-  process.stdout.write(
-    process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H',
-  );
+  if (!isCI && isInteractive && !DEBUG) {
+    process.stdout.write(
+      process.platform === 'win32' ? '\x1B[2J\x1B[0f' : '\x1B[2J\x1B[3J\x1B[H',
+    );
+  }
 }
 
 module.exports = clearConsole;
