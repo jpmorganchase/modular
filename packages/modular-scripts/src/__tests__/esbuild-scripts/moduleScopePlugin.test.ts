6de7b11b3129671f1889be978d3e623af65cfcde
Luke Sheard <luke.sheard@jpmorgan.com>
2021-11-24 10:27:17 +0000

    Implement ModuleScopePluign

---
+++ packages/modular-scripts/src/__tests__/esbuild-scripts/moduleScopePlugin.test.ts
@@ -0,0 +1,105 @@
+import * as esbuild from 'esbuild';
+import * as path from 'path';
+import * as fs from 'fs-extra';
+import stripAnsi from 'strip-ansi';
+import * as tmp from 'tmp';
+
+import plugin from '../../esbuild-scripts/plugins/moduleScopePlugin';
+import type { Paths } from '../../utils/createPaths';
+import { formatError } from '../../esbuild-scripts/utils/formatError';
+
+const emptyDir = async (dirName: string) => {
+  await fs.emptyDir(dirName);
+};
+
+describe('WHEN running esbuild with the svgrPlugin', () => {
+  describe("WHEN there's a url import", () => {
+    let tmpDir: tmp.DirResult;
+    let result: esbuild.BuildResult;
+    let outdir: string;
+
+    beforeAll(async () => {
+      tmpDir = tmp.dirSync();
+      outdir = path.join(tmpDir.name, 'output');
+      try {
+        result = await esbuild.build({
+          entryPoints: [
+            path.join(
+              __dirname,
+              '__fixtures__',
+              'module-scope',
+              'src',
+              'index.tsx',
+            ),
+          ],
+          plugins: [
+            plugin({
+              appSrc: path.join(
+                __dirname,
+                '__fixtures__',
+                'module-scope',
+                'src',
+              ),
+            } as unknown as Paths),
+          ],
+          logLevel: 'silent',
+          outdir,
+          bundle: true,
+          splitting: true,
+          format: 'esm',
+          target: 'es2021',
+        });
+      } catch (buildError) {
+        result = buildError as esbuild.BuildResult;
+      }
+    });
+
+    afterAll(async () => {
+      await emptyDir(tmpDir.name);
+      tmpDir.removeCallback();
+    });
+
+    it('SHOULD produce no warnings', () => {
+      expect(result.warnings).toMatchInlineSnapshot(`Array []`);
+    });
+
+    it('SHOULD produce an error', async () => {
+      expect(result.errors).toHaveLength(1);
+      const error = result.errors[0];
+      const { text, ...err } = error;
+      expect(err).toMatchInlineSnapshot(`
+        Object {
+          "detail": undefined,
+          "location": Object {
+            "column": 20,
+            "file": "packages/modular-scripts/src/__tests__/esbuild-scripts/__fixtures__/module-scope/src/index.tsx",
+            "length": 8,
+            "line": 1,
+            "lineText": "import { foo } from '../foo';",
+            "namespace": "",
+            "suggestion": "",
+          },
+          "notes": Array [],
+          "pluginName": "ModuleScopePlugin",
+        }
+      `);
+      expect(stripAnsi(text)).toMatchInlineSnapshot(`
+        "You attempted to import ../foo which falls outside of the project packages/modular-scripts/src/__tests__/esbuild-scripts/__fixtures__/module-scope/src directory. 
+        Relative imports outside of packages/modular-scripts/src/__tests__/esbuild-scripts/__fixtures__/module-scope/src are not supported.
+        You can either move it inside packages/modular-scripts/src/__tests__/esbuild-scripts/__fixtures__/module-scope/src, or move it to another modular package."
+      `);
+
+      const formattedError = stripAnsi(await formatError(error));
+      expect(formattedError).toMatchInlineSnapshot(`
+        "Error:[packages/modular-scripts/src/__tests__/esbuild-scripts/__fixtures__/module-scope/src/index.tsx] You attempted to import ../foo which falls outside of the project packages/modular-scripts/src/__tests__/esbuild-scripts/__fixtures__/module-scope/src directory. 
+        Relative imports outside of packages/modular-scripts/src/__tests__/esbuild-scripts/__fixtures__/module-scope/src are not supported.
+        You can either move it inside packages/modular-scripts/src/__tests__/esbuild-scripts/__fixtures__/module-scope/src, or move it to another modular package.
+          
+        > 1 | import { foo } from '../foo';
+            |                    ^
+          2 | 
+            "
+      `);
+    });
+  });
+});
---
+++ packages/modular-scripts/src/esbuild-scripts/plugins/moduleScopePlugin.ts
@@ -0,0 +1,70 @@
+import chalk from 'chalk';
+import * as esbuild from 'esbuild';
+import * as path from 'path';
+import getModularRoot from '../../utils/getModularRoot';
+import type { Paths } from '../../utils/createPaths';
+
+function createPlugin(paths: Paths): esbuild.Plugin {
+  return {
+    name: 'ModuleScopePlugin',
+    setup(build) {
+      const appSrc = paths.appSrc;
+      const relativeAppSrc = path.relative(getModularRoot(), appSrc);
+      build.onResolve({ filter: /.*/, namespace: 'file' }, (args) => {
+        if (!args.importer) {
+          return {};
+        }
+
+        if (
+          // If this resolves to a node_module, we don't care what happens next
+          args.importer.indexOf('/node_modules/') !== -1 ||
+          args.importer.indexOf('\\node_modules\\') !== -1
+        ) {
+          return {};
+        }
+        const relative = path.relative(appSrc, args.importer);
+
+        if (
+          // If it's not in one of our app src or a subdirectory, not our request!
+          relative.startsWith('../') ||
+          relative.startsWith('..\\')
+        ) {
+          return {};
+        }
+
+        const requestFullPath = path.resolve(
+          path.dirname(args.importer),
+          args.path,
+        );
+        // Find path from src to the requested file
+        // Error if in a parent directory of all given appSrcs
+        const requestRelative = path.relative(appSrc, requestFullPath);
+        if (
+          requestRelative.startsWith('../') ||
+          requestRelative.startsWith('..\\')
+        ) {
+          return {
+            errors: [
+              {
+                pluginName: 'ModuleScopePlugin',
+                text: `You attempted to import ${chalk.cyan(
+                  args.path,
+                )} which falls outside of the project ${chalk.cyan(
+                  relativeAppSrc,
+                )} directory. 
+Relative imports outside of ${chalk.cyan(relativeAppSrc)} are not supported.
+You can either move it inside ${chalk.cyan(
+                  relativeAppSrc,
+                )}, or move it to another ${chalk.cyan('modular')} package.`,
+              },
+            ],
+          };
+        } else {
+          return {};
+        }
+      });
+    },
+  };
+}
+
+export default createPlugin;
