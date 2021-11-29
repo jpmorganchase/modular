fb383e38782a744bb76bf8fd87970698615e4c37
Luke Sheard <luke.sheard@jpmorgan.com>
2021-11-29 22:59:29 +0000

    esbuild improvements

---
+++ packages/modular-scripts/src/esbuild-scripts/start/plugins/incrementalReporter.ts
@@ -0,0 +1,43 @@
+import { Plugin } from 'esbuild';
+import chalk from 'chalk';
+import * as logger from '../../../utils/logger';
+import { formatError } from '../../utils/formatError';
+import type { Paths } from '../../../utils/createPaths';
+
+function createPlugin(paths: Paths): Plugin {
+  const plugin: Plugin = {
+    name: 'incremental-errors',
+    setup(build) {
+      build.onEnd(async (result) => {
+        if (result.errors.length) {
+          logger.log(chalk.red('Failed to compile.\n'));
+        } else {
+          if (result.warnings.length) {
+            logger.log(chalk.red('Compiled with warnings.\n'));
+          }
+        }
+
+        // If errors exist, only show errors.
+        if (result.errors.length) {
+          await Promise.all(
+            result.errors.map(async (m) => {
+              logger.log(await formatError(m, paths.appPath));
+            }),
+          );
+        }
+
+        if (result.warnings.length) {
+          await Promise.all(
+            result.warnings.map(async (m) => {
+              logger.log(await formatError(m, paths.appPath));
+            }),
+          );
+        }
+      });
+    },
+  };
+
+  return plugin;
+}
+
+export default createPlugin;
---
+++ packages/modular-scripts/src/esbuild-scripts/start/plugins/wsReload.ts
@@ -0,0 +1,57 @@
+import esbuild from 'esbuild';
+import * as ws from 'ws';
+import { formatError } from '../../utils/formatError';
+import type { Paths } from '../../../utils/createPaths';
+
+export default function createPlugin(
+  name: string,
+  server: ws.Server,
+  paths: Paths,
+): esbuild.Plugin {
+  const plugin: esbuild.Plugin = {
+    name: 'ws-reload',
+    setup(build) {
+      let building = false;
+      let result: esbuild.BuildResult;
+      const { appPath } = paths;
+
+      const publish = () => {
+        // eslint-disable-next-line @typescript-eslint/no-misused-promises
+        server.clients.forEach(async (socket) => {
+          socket.send(
+            JSON.stringify({
+              name,
+              building,
+              result: {
+                errors: await Promise.all(
+                  result.errors.map((error) => formatError(error, appPath)),
+                ),
+                warnings: await Promise.all(
+                  result.warnings.map((warning) =>
+                    formatError(warning, appPath),
+                  ),
+                ),
+              },
+            }),
+          );
+        });
+      };
+
+      server.on('connection', () => {
+        publish();
+      });
+
+      build.onStart(() => {
+        building = true;
+        publish();
+      });
+      build.onEnd((_result) => {
+        building = false;
+        result = _result;
+
+        publish();
+      });
+    },
+  };
+  return plugin;
+}
