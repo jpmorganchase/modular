55ee463881e84798b77689116e33401756de1301
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2021-12-13 15:32:53 +0000

    Add tests

---
+++ packages/modular-scripts/src/__tests__/rename.test.ts
@@ -0,0 +1,170 @@
+import execa from 'execa';
+import { promisify } from 'util';
+import _rimraf from 'rimraf';
+import path from 'path';
+import fs from 'fs-extra';
+
+import getModularRoot from '../utils/getModularRoot';
+
+const rimraf = promisify(_rimraf);
+
+const modularRoot = getModularRoot();
+
+function modular(str: string, opts: Record<string, unknown> = {}) {
+  return execa('yarnpkg', ['modular', ...str.split(' ')], {
+    cwd: modularRoot,
+    cleanup: true,
+    ...opts,
+  });
+}
+
+async function cleanup() {
+  const packagesPath = path.join(getModularRoot(), 'packages');
+
+  await rimraf(path.join(packagesPath, 'sample-depending-package'));
+  await rimraf(path.join(packagesPath, 'sample-library-package'));
+  await rimraf(path.join(packagesPath, 'sample-library-renamed-package'));
+  // run yarn so yarn.lock gets reset
+  await execa('yarnpkg', ['--silent'], {
+    cwd: modularRoot,
+  });
+}
+
+beforeAll(async () => {
+  await cleanup();
+});
+
+afterAll(async () => {
+  await cleanup();
+});
+
+describe('Rename command', () => {
+  beforeAll(async () => {
+    await modular(
+      'add sample-depending-package --unstable-type package --unstable-name sample-depending-package',
+      {
+        stdio: 'inherit',
+      },
+    );
+
+    await modular(
+      'add sample-library-package --unstable-type package --unstable-name sample-library-package',
+      {
+        stdio: 'inherit',
+      },
+    );
+
+    const libraryPackageSrc = path.join(
+      getModularRoot(),
+      'packages',
+      'sample-library-package',
+      'src',
+    );
+    await fs.emptyDir(libraryPackageSrc);
+    await fs.copy(
+      path.join(
+        __dirname,
+        '__fixtures__',
+        'packages',
+        'sample-library-package',
+      ),
+      libraryPackageSrc,
+    );
+
+    const dependingPackageSrc = path.join(
+      getModularRoot(),
+      'packages',
+      'sample-depending-package',
+      'src',
+    );
+    await fs.emptyDir(dependingPackageSrc);
+    await fs.copy(
+      path.join(
+        __dirname,
+        '__fixtures__',
+        'packages',
+        'sample-depending-package',
+      ),
+      dependingPackageSrc,
+    );
+
+    const dependingPackageJsonPath = path.join(
+      getModularRoot(),
+      'packages',
+      'sample-depending-package',
+      'package.json',
+    );
+
+    const packageJson = (await fs.readJson(dependingPackageJsonPath)) as {
+      dependencies: Record<string, string>;
+    };
+    packageJson.dependencies = Object.assign(packageJson.dependencies || {}, {
+      'sample-library-package': '1.0.0',
+    });
+    await fs.writeJson(dependingPackageJsonPath, packageJson, { spaces: 2 });
+  });
+
+  it('expects package.json and file importing the dependency to refer to the renamed dep', async () => {
+    await modular(
+      'rename sample-library-package sample-library-renamed-package',
+      {
+        stdio: 'inherit',
+      },
+    );
+    expect(
+      await fs.readJson(
+        path.join(
+          modularRoot,
+          'packages',
+          'sample-depending-package',
+          'package.json',
+        ),
+      ),
+    ).toMatchInlineSnapshot(`
+      Object {
+        "dependencies": Object {
+          "sample-library-renamed-package": "1.0.0",
+        },
+        "files": Array [
+          "README.md",
+        ],
+        "license": "UNLICENSED",
+        "main": "src/index.ts",
+        "name": "sample-depending-package",
+        "version": "1.0.0",
+      }
+    `);
+
+    expect(
+      await fs.readFile(
+        path.join(
+          modularRoot,
+          'packages',
+          'sample-depending-package',
+          'src',
+          'App.tsx',
+        ),
+        'utf8',
+      ),
+    ).toMatchInlineSnapshot(`
+      "import * as React from 'react';
+      // @ts-ignore
+      import summer from 'sample-library-renamed-package';
+
+      declare function summer(a: number, b: number): number;
+      function App(): JSX.Element {
+        const result = summer(7, 7);
+        return (
+          <div className=\\"App\\">
+            <header className=\\"App-header\\">
+              <p>This is the result: {result}</p>
+            </header>
+          </div>
+        );
+      }
+
+      export default App;
+      "
+    `);
+  });
+});
