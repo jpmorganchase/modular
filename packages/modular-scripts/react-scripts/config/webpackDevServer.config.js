50c93b3be3278cae12b07c9b47c4a099a42a9c5e
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-01-18 18:17:00 +0000

    Revert "Fix deprecation"
    This reverts commit 7835e8ea4910ee547266305ac985f5f7b515b612.

--- packages/modular-scripts/react-scripts/config/webpackDevServer.config.js
+++ packages/modular-scripts/react-scripts/config/webpackDevServer.config.js
@@ -94,29 +94,31 @@ module.exports = function (port, proxy, allowedHost) {
     },
     // `proxy` is run between `before` and `after` `webpack-dev-server` hooks
     proxy,
-    setupMiddlewares(middlewares, server) {
+    onBeforeSetupMiddleware(server) {
       const app = server.app;
       // Keep `evalSourceMapMiddleware` and `errorOverlayMiddleware`
       // middlewares before `redirectServedPath` otherwise will not have any effect
       // This lets us fetch source contents from webpack for the error overlay
-      middlewares.unshift(evalSourceMapMiddleware(server));
+      app.use(evalSourceMapMiddleware(server));
       // This lets us open files from the runtime error overlay.
-      middlewares.unshift(errorOverlayMiddleware());
+      app.use(errorOverlayMiddleware());
 
       if (fs.existsSync(paths.proxySetup)) {
         // This registers user provided middleware for proxy reasons
         require(paths.proxySetup)(app);
       }
-
+    },
+    onAfterSetupMiddleware(server) {
+      const app = server.app;
       // Redirect to `PUBLIC_URL` or `homepage` from `package.json` if url not match
-      middlewares.push(redirectServedPath(paths.publicUrlOrPath));
+      app.use(redirectServedPath(paths.publicUrlOrPath));
 
       // This service worker file is effectively a 'no-op' that will reset any
       // previous service worker registered for the same host:port combination.
       // We do this in development to avoid hitting the production cache if
       // it used the same host and port.
       // https://github.com/facebook/create-react-app/issues/2272#issuecomment-302832432
-      middlewares.push(noopServiceWorkerMiddleware(paths.publicUrlOrPath));
+      app.use(noopServiceWorkerMiddleware(paths.publicUrlOrPath));
     },
   };
 };
