b39e4967d899d7623211f8b1411123b19466d992
Luke Sheard <LukeSheard@users.noreply.github.com>
2022-01-24 12:13:07 +0000

    Bugfix/release build (#1324)
    * fix babel build for packages
    
    * add build as a command when statically checking modular

--- babel.config.js
+++ babel.config.js
@@ -3,12 +3,6 @@
 
 'use strict';
 
-const semver = require('semver');
-const pkgJson = require('./package.json');
-
-// Copied from https://github.com/facebook/jest/blob/56782b9/babel.config.js#L11
-const supportedNodeVersion = semver.minVersion(pkgJson.engines.node).version;
-
 module.exports = (api) => {
   api.cache(true);
 
@@ -18,7 +12,7 @@ module.exports = (api) => {
         '@babel/preset-env',
         {
           targets: {
-            node: supportedNodeVersion,
+            node: 'current',
           },
         },
       ],
