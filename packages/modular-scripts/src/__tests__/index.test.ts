d46627f4889c6d14ba75fa6dbd8de7b1c1c0e8b9
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-02-14 16:13:00 +0000

    Fix view tests port

--- packages/modular-scripts/src/__tests__/index.test.ts
+++ packages/modular-scripts/src/__tests__/index.test.ts
@@ -151,7 +151,7 @@ describe('modular-scripts', () => {
       };
 
       browser = await puppeteer.launch(launchArgs);
-      port = '4000';
+      port = '3000';
       devServer = await startApp(targetedView, { env: { PORT: port } });
     });
 
