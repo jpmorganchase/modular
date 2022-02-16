ff1f713a89c1eca795398cf359abd90c17e5a5af
Luke Sheard <me@lukesheard.com>
2022-02-16 14:00:35 +0000

    fix tests 4

--- packages/modular-scripts/src/__tests__/index.test.ts
+++ packages/modular-scripts/src/__tests__/index.test.ts
@@ -100,7 +100,6 @@ describe('modular-scripts', () => {
       expect(tree(path.join(packagesPath, 'sample-view')))
         .toMatchInlineSnapshot(`
         "sample-view
-        ├─ README.md #11adaka
         ├─ package.json
         └─ src
            ├─ __tests__
@@ -113,7 +112,6 @@ describe('modular-scripts', () => {
       expect(tree(path.join(packagesPath, 'sample-package')))
         .toMatchInlineSnapshot(`
         "sample-package
-        ├─ README.md #1jv3l2q
         ├─ package.json
         └─ src
            ├─ __tests__
@@ -127,7 +125,6 @@ describe('modular-scripts', () => {
       expect(tree(path.join(packagesPath, 'nested/sample-nested-package')))
         .toMatchInlineSnapshot(`
         "sample-nested-package
-        ├─ README.md #1jv3l2q
         ├─ package.json
         └─ src
            ├─ __tests__
@@ -214,18 +211,18 @@ describe('modular-scripts', () => {
             "react": "17.0.2",
           },
           "files": Array [
-            "README.md",
             "dist-cjs",
             "dist-es",
             "dist-types",
+            "README.md",
           ],
-          "license": "UNLICENSED",
           "main": "dist-cjs/index.js",
           "modular": Object {
             "type": "view",
           },
           "module": "dist-es/index.js",
           "name": "sample-view",
+          "private": false,
           "typings": "dist-types/index.d.ts",
           "version": "1.0.0",
         }
@@ -266,7 +263,6 @@ describe('modular-scripts', () => {
       expect(tree(path.join(modularRoot, 'dist', 'sample-view')))
         .toMatchInlineSnapshot(`
         "sample-view
-        ├─ README.md #11adaka
         ├─ dist-cjs
         │  ├─ index.js #a7k6ic
         │  └─ index.js.map #1825qkv
@@ -328,15 +324,18 @@ describe('modular-scripts', () => {
         Object {
           "dependencies": Object {},
           "files": Array [
-            "README.md",
             "dist-cjs",
             "dist-es",
             "dist-types",
+            "README.md",
           ],
-          "license": "UNLICENSED",
           "main": "dist-cjs/index.js",
+          "modular": Object {
+            "type": "package",
+          },
           "module": "dist-es/index.js",
           "name": "sample-package",
+          "private": false,
           "typings": "dist-types/index.d.ts",
           "version": "1.0.0",
         }
@@ -347,7 +346,6 @@ describe('modular-scripts', () => {
       expect(tree(path.join(modularRoot, 'dist', 'sample-package')))
         .toMatchInlineSnapshot(`
         "sample-package
-        ├─ README.md #1jv3l2q
         ├─ dist-cjs
         │  ├─ index.js #rq9uxe
         │  └─ index.js.map #17f4e4r
@@ -398,15 +396,18 @@ describe('modular-scripts', () => {
         Object {
           "dependencies": Object {},
           "files": Array [
-            "README.md",
             "dist-cjs",
             "dist-es",
             "dist-types",
+            "README.md",
           ],
-          "license": "UNLICENSED",
           "main": "dist-cjs/nested-sample-package.cjs.js",
+          "modular": Object {
+            "type": "package",
+          },
           "module": "dist-es/nested-sample-package.es.js",
           "name": "@nested/sample-package",
+          "private": false,
           "typings": "dist-types/index.d.ts",
           "version": "1.0.0",
         }
@@ -417,7 +418,6 @@ describe('modular-scripts', () => {
       expect(tree(path.join(modularRoot, 'dist', 'nested-sample-package')))
         .toMatchInlineSnapshot(`
         "nested-sample-package
-        ├─ README.md #1jv3l2q
         ├─ dist-cjs
         │  ├─ nested-sample-package.cjs.js #kv2xzp
         │  └─ nested-sample-package.cjs.js.map #wxcsbr
