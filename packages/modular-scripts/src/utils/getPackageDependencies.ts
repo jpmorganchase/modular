33b63f21a390a396fb140d03cb436e1b8aea864d
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-02-17 11:20:19 +0000

    Revert memoizing gpd

--- packages/modular-scripts/src/utils/getPackageDependencies.ts
+++ packages/modular-scripts/src/utils/getPackageDependencies.ts
@@ -5,7 +5,6 @@ import type { CoreProperties } from '@schemastore/package';
 import getModularRoot from './getModularRoot';
 import getLocation from './getLocation';
 import getWorkspaceInfo from './getWorkspaceInfo';
-import memoize from './memoize';
 
 type DependencyManifest = NonNullable<CoreProperties['dependencies']>;
 
@@ -38,49 +37,47 @@ function getDependenciesFromSource(workspaceLocation: string) {
   return Array.from(dependencySet);
 }
 
-export const getPackageDependencies = memoize(
-  async function getPackageDependencies(
-    target: string,
-  ): Promise<DependencyManifest> {
-    /* This function is based on the assumption that nested package are not supported, so dependencies can be either declared in the
-     * target's package.json or hoisted up to the workspace root.
-     */
-    const targetLocation = await getLocation(target);
-    const workspaceInfo = getWorkspaceInfo();
+export async function getPackageDependencies(
+  target: string,
+): Promise<DependencyManifest> {
+  /* This function is based on the assumption that nested package are not supported, so dependencies can be either declared in the
+   * target's package.json or hoisted up to the workspace root.
+   */
+  const targetLocation = await getLocation(target);
+  const workspaceInfo = getWorkspaceInfo();
 
-    const rootPackageJsonDependencies =
-      (
-        fs.readJSONSync(
-          path.join(getModularRoot(), 'package.json'),
-        ) as CoreProperties
-      ).dependencies || {};
+  const rootPackageJsonDependencies =
+    (
+      fs.readJSONSync(
+        path.join(getModularRoot(), 'package.json'),
+      ) as CoreProperties
+    ).dependencies || {};
 
-    const targetPackageJsonDependencies =
-      (
-        fs.readJSONSync(
-          path.join(targetLocation, 'package.json'),
-        ) as CoreProperties
-      ).dependencies || {};
+  const targetPackageJsonDependencies =
+    (
+      fs.readJSONSync(
+        path.join(targetLocation, 'package.json'),
+      ) as CoreProperties
+    ).dependencies || {};
 
-    /* Get regular dependencies from package.json (regular) or root package.json (hoisted)
-     * Exclude workspace dependencies. Error if a dependency is imported in the source code
-     * but not specified in any of the package.jsons
-     */
-    const manifest = getDependenciesFromSource(targetLocation)
-      .filter((depName) => !(depName in workspaceInfo))
-      .reduce<DependencyManifest>((manifest, depName) => {
-        const depVersion =
-          targetPackageJsonDependencies[depName] ??
-          rootPackageJsonDependencies[depName];
-        if (!depVersion) {
-          throw new Error(
-            `Package ${depName} imported in ${target} source but not found in package dependencies or hoisted dependencies`,
-          );
-        }
-        manifest[depName] = depVersion;
-        return manifest;
-      }, {});
+  /* Get regular dependencies from package.json (regular) or root package.json (hoisted)
+   * Exclude workspace dependencies. Error if a dependency is imported in the source code
+   * but not specified in any of the package.jsons
+   */
+  const manifest = getDependenciesFromSource(targetLocation)
+    .filter((depName) => !(depName in workspaceInfo))
+    .reduce<DependencyManifest>((manifest, depName) => {
+      const depVersion =
+        targetPackageJsonDependencies[depName] ??
+        rootPackageJsonDependencies[depName];
+      if (!depVersion) {
+        throw new Error(
+          `Package ${depName} imported in ${target} source but not found in package dependencies or hoisted dependencies`,
+        );
+      }
+      manifest[depName] = depVersion;
+      return manifest;
+    }, {});
 
-    return manifest;
-  },
-);
+  return manifest;
+}
