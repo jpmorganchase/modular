9ad46d80eae03068c2321b30a5ebd0f16d08016f
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-03-03 16:16:51 +0000

    Rewrite global css

--- packages/modular-scripts/src/esbuild-scripts/plugins/rewriteDependenciesPlugin.ts
+++ packages/modular-scripts/src/esbuild-scripts/plugins/rewriteDependenciesPlugin.ts
@@ -23,6 +23,8 @@ export function createRewriteDependenciesPlugin(
   const dependencyRewritePlugin: esbuild.Plugin = {
     name: 'dependency-rewrite',
     setup(build) {
+      // Don't want to load global css more than once
+      const globalCSSMap: Map<string, boolean> = new Map();
       // Filter on external dependencies
       build.onResolve(
         { filter: /^[a-z0-9-~]|@/, namespace: 'file' },
@@ -31,18 +33,56 @@ export function createRewriteDependenciesPlugin(
           const { dependencyName, submodule } = parsePackageName(args.path);
           // Find dependency name (no submodule) in the pre-built import map
           if (dependencyName in importMap) {
+            // Rewrite the path taking the submodule into account
+            const path = `${importMap[dependencyName]}${
+              submodule ? `/${submodule}` : ''
+            }`;
+            if (submodule.endsWith('.css')) {
+              // This is a global CSS import from the CDN.
+              // We want to ignore this import if it's been already imported before (no need to inject it twice into the HEAD)
+              const namespace = globalCSSMap.get(path)
+                ? 'rewritable-css-import-ignore'
+                : 'rewritable-css-import';
+              // Set it in the "allready done" map
+              globalCSSMap.set(path, true);
+              return {
+                path,
+                namespace,
+              };
+            }
             // Just rewrite and mark as external. It will be ignored the next resolve cycle
             return {
-              // If there's a submodule, concatenate it in
-              path: `${importMap[dependencyName]}${
-                submodule ? `/${submodule}` : ''
-              }`,
+              path,
               external: true,
             } as esbuild.OnResolveResult;
           } else {
             // Dependency has been filtered out: ignore and bundle
             return {};
           }
+        },
+      );
+      build.onLoad(
+        { filter: /^[a-z0-9-~]|@/, namespace: 'rewritable-css-import' },
+        (args) => {
+          return {
+            contents: `
+          const link = document.createElement('link');
+          link.rel = 'stylesheet'; 
+          link.type = 'text/css';
+          link.href = '${args.path}'; 
+          document.getElementsByTagName('HEAD')[0].appendChild(link); 
+          `,
+          };
+        },
+      );
+      build.onLoad(
+        { filter: /^[a-z0-9-~]|@/, namespace: 'rewritable-css-import-ignore' },
+        (args) => {
+          return {
+            contents: `
+            /* Ignored CSS import at path ${args.path} */
+            `,
+          };
         },
       );
     },
