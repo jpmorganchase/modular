bc9cb4c3904445190b26aa685afc3f079d147b68
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-03-11 13:19:07 +0000

    Remove unwanted media dist files

--- packages/modular-scripts/react-scripts/config/webpack.config.js
+++ packages/modular-scripts/react-scripts/config/webpack.config.js
@@ -452,7 +452,7 @@ module.exports = function (webpackEnv) {
               // its runtime that would otherwise be processed through "file" loader.
               // Also exclude `html` and `json` extensions so they get processed
               // by webpacks internal loaders.
-              exclude: [/\.(js|mjs|jsx|ts|tsx)$/, /\.html$/, /\.json$/],
+              exclude: [/(^|\.(js|mjs|jsx|ts|tsx|html|json))$/],
               options: {
                 name: 'static/media/[name].[hash:8].[ext]',
               },
