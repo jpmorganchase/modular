e38289a3edde0d1167bb5c5393f3ec9b61bdf875
Pryke, Benjamin <benjamin.pryke@jpmorgan.com>
2022-03-17 15:46:38 +0000

    test: add tests for --private option

--- packages/modular-scripts/src/__tests__/rename.test.ts
+++ packages/modular-scripts/src/__tests__/rename.test.ts
@@ -1,157 +1,64 @@
-import execa from 'execa';
-import { promisify } from 'util';
-import _rimraf from 'rimraf';
-import path from 'path';
 import fs from 'fs-extra';
+import path from 'path';
 
+import { addFixturePackage, cleanup, modular } from '../test/utils';
 import getModularRoot from '../utils/getModularRoot';
-
-const rimraf = promisify(_rimraf);
 
 const modularRoot = getModularRoot();
 
-function modular(str: string, opts: Record<string, unknown> = {}) {
-  return execa('yarnpkg', ['modular', ...str.split(' ')], {
-    cwd: modularRoot,
-    cleanup: true,
-    ...opts,
-  });
-}
-
-async function cleanup() {
-  const packagesPath = path.join(getModularRoot(), 'packages');
-
-  await rimraf(path.join(packagesPath, 'sample-depending-package'));
-  await rimraf(path.join(packagesPath, 'sample-library-package'));
-  // run yarn so yarn.lock gets reset
-  await execa('yarnpkg', ['--silent'], {
-    cwd: modularRoot,
-  });
-}
-
-beforeAll(async () => {
-  await cleanup();
-});
-
-afterAll(async () => {
-  await cleanup();
-});
-
 describe('Rename command', () => {
+  const libraryPackage = 'sample-renamable-library-package';
+  const renamedPackage = 'sample-renamed-library-package';
+  const dependentPackage = 'sample-renamable-depending-package';
+
   beforeAll(async () => {
-    await modular(
-      'add sample-depending-package --unstable-type package --unstable-name sample-depending-package',
-      {
-        stdio: 'inherit',
-      },
-    );
+    await cleanup([dependentPackage, libraryPackage, renamedPackage]);
+    await addFixturePackage(libraryPackage, { copy: false });
+    await addFixturePackage(dependentPackage, { copy: false });
 
-    await modular(
-      'add sample-library-package --unstable-type package --unstable-name sample-library-package',
-      {
-        stdio: 'inherit',
-      },
-    );
-
-    const libraryPackageSrc = path.join(
-      getModularRoot(),
-      'packages',
-      'sample-library-package',
-      'src',
-    );
-    await fs.emptyDir(libraryPackageSrc);
-    await fs.copy(
-      path.join(
-        __dirname,
-        '__fixtures__',
-        'packages',
-        'sample-library-package',
-      ),
-      libraryPackageSrc,
-    );
-
-    const dependingPackageSrc = path.join(
-      getModularRoot(),
-      'packages',
-      'sample-depending-package',
-      'src',
-    );
-    await fs.emptyDir(dependingPackageSrc);
-    await fs.copy(
-      path.join(
-        __dirname,
-        '__fixtures__',
-        'packages',
-        'sample-depending-package',
-      ),
-      dependingPackageSrc,
-    );
-
-    // Can't specify it in the fixtures, otherwise rename will rename the 'sample-library-package' dependency!
-    const app_tsx = `
-  import * as React from 'react';
-  import summer from 'sample-library-package';
-
-  declare function summer(a: number, b: number): number;
-  function App(): JSX.Element {
-    const result = summer(7, 7);
-    return (
-      <div className="App">
-        <header className="App-header">
-          <p>This is the result: {result}</p>
-        </header>
-      </div>
-    );
-  }
-
-  export default App;
-`;
-
+    // For now, we have to write the index.ts file here because if we store it
+    // as a fixture, the fixture itself will be renamed during our test
     await fs.writeFile(
-      path.join(dependingPackageSrc, 'App.tsx'),
-      app_tsx,
-      'utf8',
+      path.join(modularRoot, 'packages', dependentPackage, 'src', 'index.ts'),
+      `/* eslint-disable @typescript-eslint/no-unsafe-call */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
+// @ts-ignore
+import add from '${libraryPackage}';
+
+export function double(x: number): number {
+  return add(x, x);
+}
+`,
     );
   });
+
+  afterAll(
+    async () =>
+      await cleanup([dependentPackage, libraryPackage, renamedPackage]),
+  );
 
   it('expects file importing the dependency to refer to the renamed dep', async () => {
-    await modular(
-      'rename sample-library-package sample-library-renamed-package',
-      {
-        stdio: 'inherit',
-      },
-    );
+    await modular(`rename ${libraryPackage} ${renamedPackage}`, {
+      stdio: 'inherit',
+    });
+
+    const snapshot = `
+      "/* eslint-disable @typescript-eslint/no-unsafe-call */
+      /* eslint-disable @typescript-eslint/no-unsafe-return */
+      // @ts-ignore
+      import add from '${renamedPackage}';
+      
+      export function double(x: number): number {
+        return add(x, x);
+      }
+      "
+    `;
 
     expect(
       await fs.readFile(
-        path.join(
-          modularRoot,
-          'packages',
-          'sample-depending-package',
-          'src',
-          'App.tsx',
-        ),
+        path.join(modularRoot, 'packages', dependentPackage, 'src', 'index.ts'),
         'utf8',
       ),
-    ).toMatchInlineSnapshot(`
-      "
-        import * as React from 'react';
-        import summer from 'sample-library-renamed-package';
-
-        declare function summer(a: number, b: number): number;
-        function App(): JSX.Element {
-          const result = summer(7, 7);
-          return (
-            <div className=\\"App\\">
-              <header className=\\"App-header\\">
-                <p>This is the result: {result}</p>
-              </header>
-            </div>
-          );
-        }
-
-        export default App;
-      "
-    `);
+    ).toMatchInlineSnapshot(snapshot);
   });
 });
---
+++ packages/modular-scripts/src/test/utils.ts
@@ -0,0 +1,55 @@
+import _rimraf from 'rimraf';
+import execa from 'execa';
+import fs from 'fs-extra';
+import path from 'path';
+import { promisify } from 'util';
+
+import getModularRoot from '../utils/getModularRoot';
+
+const rimraf = promisify(_rimraf);
+
+const modularRoot = getModularRoot();
+
+export function modular(
+  str: string,
+  opts: Record<string, unknown> = {},
+): execa.ExecaChildProcess<string> {
+  return execa('yarnpkg', ['modular', ...str.split(' ')], {
+    cwd: modularRoot,
+    cleanup: true,
+    ...opts,
+  });
+}
+
+export async function cleanup(packageNames: Array<string>): Promise<void> {
+  const packagesPath = path.join(modularRoot, 'packages');
+  const distPath = path.join(modularRoot, 'dist');
+
+  for (const packageName of packageNames) {
+    await rimraf(path.join(packagesPath, packageName));
+    await rimraf(path.join(distPath, packageName));
+  }
+
+  // run yarn so yarn.lock gets reset
+  await execa('yarnpkg', ['--silent'], {
+    cwd: modularRoot,
+  });
+}
+
+export async function addFixturePackage(
+  name: string,
+  options: { copy: boolean } = { copy: true },
+): Promise<void> {
+  const packageSrcDir = path.join(modularRoot, 'packages', name, 'src');
+  await modular(`add ${name} --unstable-type package --unstable-name ${name}`, {
+    stdio: 'inherit',
+  });
+  await fs.emptyDir(packageSrcDir);
+
+  if (options.copy) {
+    await fs.copy(
+      path.join(__dirname, '..', '__tests__', '__fixtures__', 'packages', name),
+      packageSrcDir,
+    );
+  }
+}
