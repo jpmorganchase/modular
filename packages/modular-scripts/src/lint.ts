f7e7ebdc644b11dc95e8dfd9eb86a261551c331a
Kathan Cheema <cheemcheem@users.noreply.github.com>
2022-03-25 18:53:13 +0000

    Fix bug that prevents lint diff code from running

--- packages/modular-scripts/src/lint.ts
+++ packages/modular-scripts/src/lint.ts
@@ -23,7 +23,7 @@ async function lint(
   const lintExtensions = ['.ts', '.tsx', '.js', '.jsx'];
   let targetedFiles = ['<rootDir>/**/src/**/*.{js,jsx,ts,tsx}'];
 
-  if (!all && !isCI && !regexes) {
+  if (!all && !isCI && regexes.length === 0) {
     const diffedFiles = getDiffedFiles();
     if (diffedFiles.length === 0) {
       logger.log(
