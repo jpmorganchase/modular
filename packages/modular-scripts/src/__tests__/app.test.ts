7c57c040aa344bfe590c0a582e8420818d9d7c1e
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-05-30 10:29:25 +0100

    Fix snaps

--- packages/modular-scripts/src/__tests__/app.test.ts
+++ packages/modular-scripts/src/__tests__/app.test.ts
@@ -130,9 +130,9 @@ describe('When working with a nested app', () => {
     expect(tree(path.join(modularRoot, 'dist', 'scoped-sample-app')))
       .toMatchInlineSnapshot(`
       "scoped-sample-app
-      ├─ asset-manifest.json #hy4ikp
+      ├─ asset-manifest.json #1pcwjot
       ├─ favicon.ico #6pu3rg
-      ├─ index.html #idgpeh
+      ├─ index.html #bs7ctd
       ├─ logo192.png #1nez7vk
       ├─ logo512.png #1hwqvcc
       ├─ manifest.json #19gah8o
@@ -146,8 +146,8 @@ describe('When working with a nested app', () => {
          │  ├─ 788.bbd34b33.js #33pg04
          │  ├─ 788.bbd34b33.js.LICENSE.txt #eplx8h
          │  ├─ 788.bbd34b33.js.map #1xdy7n0
-         │  ├─ main.1f260474.js #3ojt7i
-         │  ├─ main.1f260474.js.map #881te9
+         │  ├─ main.99649537.js #b22d4x
+         │  ├─ main.99649537.js.map #16cngow
          │  ├─ runtime-main.de012fdc.js #1qz643h
          │  └─ runtime-main.de012fdc.js.map #ntuwq4
          └─ media
@@ -247,12 +247,12 @@ describe('When working with a nested app', () => {
               'scoped-sample-app',
               'static',
               'js',
-              'main.1f260474.js',
+              'main.99649537.js',
             ),
           ),
         ),
         {
-          filepath: 'main.1f260474.js',
+          filepath: 'main.99649537.js',
         },
       ),
     ).toMatchSnapshot();
@@ -347,9 +347,9 @@ describe('when working with an app', () => {
     expect(tree(path.join(modularRoot, 'dist', 'sample-app')))
       .toMatchInlineSnapshot(`
       "sample-app
-      ├─ asset-manifest.json #y1bfdl
+      ├─ asset-manifest.json #lp70k5
       ├─ favicon.ico #6pu3rg
-      ├─ index.html #3u8gor
+      ├─ index.html #afmhhb
       ├─ logo192.png #1nez7vk
       ├─ logo512.png #1hwqvcc
       ├─ manifest.json #19gah8o
@@ -363,8 +363,8 @@ describe('when working with an app', () => {
          │  ├─ 316.394ef80b.js #1mv4xg9
          │  ├─ 316.394ef80b.js.LICENSE.txt #eplx8h
          │  ├─ 316.394ef80b.js.map #b33pzn
-         │  ├─ main.b0eac4c5.js #169xo7t
-         │  ├─ main.b0eac4c5.js.map #19epn0f
+         │  ├─ main.6146e428.js #1d7hazv
+         │  ├─ main.6146e428.js.map #hkmqfm
          │  ├─ runtime-main.e92969dd.js #1is98ey
          │  └─ runtime-main.e92969dd.js.map #19haxsp
          └─ media
@@ -434,12 +434,12 @@ describe('when working with an app', () => {
               'sample-app',
               'static',
               'js',
-              'main.b0eac4c5.js',
+              'main.6146e428.js',
             ),
           ),
         ),
         {
-          filepath: 'main.b0eac4c5.js',
+          filepath: 'main.6146e428.js',
         },
       ),
     ).toMatchSnapshot();
