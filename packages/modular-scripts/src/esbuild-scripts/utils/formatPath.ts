e3809a87be90e38aeee265809f6e20d8a8808c1e
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2022-07-05 10:08:08 +0100

    Always format from Win32 to Posix

--- packages/modular-scripts/src/esbuild-scripts/utils/formatPath.ts
+++ packages/modular-scripts/src/esbuild-scripts/utils/formatPath.ts
@@ -2,6 +2,6 @@ import path from 'path';
 
 export function normalizeToPosix<T extends string | undefined>(pathName: T): T {
   return (
-    pathName ? pathName.split(path.sep).join(path.posix.sep) : pathName
+    pathName ? pathName.split(path.win32.sep).join(path.posix.sep) : pathName
   ) as T;
 }
