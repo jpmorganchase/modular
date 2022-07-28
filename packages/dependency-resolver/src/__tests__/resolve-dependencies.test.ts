60406a73180a529cf8fbe72389483ad4a6300154
Cristiano Belloni <cristiano@hya.io>
2022-07-28 18:28:15 +0100

    Add cycle detection and DFS

--- packages/dependency-resolver/src/__tests__/resolve-dependencies.test.ts
+++ packages/dependency-resolver/src/__tests__/resolve-dependencies.test.ts
@@ -1,8 +1,8 @@
-import { flattenWorkspaceDependencies, LiteWorkSpaceRecord } from '../';
+import { walkWorkspaceDependencies, LiteWorkSpaceRecord } from '../';
 
 describe('@modular-scripts/dependency-resolver', () => {
-  describe('flatten dependencies', () => {
-    it('flattens dependencies', () => {
+  describe('resolve dependencies', () => {
+    it('resolves descendants in order', () => {
       const workspaces: Record<string, LiteWorkSpaceRecord> = {
         a: { location: '/a', workspaceDependencies: ['b', 'c'] },
         b: { location: '/b', workspaceDependencies: ['d'] },
@@ -10,26 +10,50 @@ describe('@modular-scripts/dependency-resolver', () => {
         d: { location: '/d', workspaceDependencies: undefined },
         e: { location: '/e', workspaceDependencies: ['a', 'b', 'c'] },
       };
-      const flattenedDependencies = flattenWorkspaceDependencies(workspaces);
-      expect(flattenedDependencies.get('a')).toEqual(new Set(['b', 'c', 'd']));
-      expect(flattenedDependencies.get('b')).toEqual(new Set(['d']));
-      expect(flattenedDependencies.get('c')).toEqual(new Set(['b', 'd']));
-      expect(flattenedDependencies.get('d')).toEqual(new Set());
-      expect(flattenedDependencies.get('e')).toEqual(
-        new Set(['a', 'b', 'c', 'd']),
+      expect(walkWorkspaceDependencies(workspaces, 'a')).toEqual(
+        new Map([
+          ['c', 1],
+          ['b', 2],
+          ['d', 3],
+        ]),
+      );
+      expect(walkWorkspaceDependencies(workspaces, 'b')).toEqual(
+        new Map([['d', 1]]),
+      );
+      expect(walkWorkspaceDependencies(workspaces, 'c')).toEqual(
+        new Map([
+          ['b', 1],
+          ['d', 2],
+        ]),
+      );
+      expect(walkWorkspaceDependencies(workspaces, 'd')).toEqual(new Map());
+      expect(walkWorkspaceDependencies(workspaces, 'e')).toEqual(
+        new Map([
+          ['a', 1],
+          ['b', 3],
+          ['c', 2],
+          ['d', 4],
+        ]),
       );
     });
 
-    it('flags loops', () => {
+    it('throws on cycles going back to the first dependency', () => {
       const workspaces: Record<string, LiteWorkSpaceRecord> = {
         a: { location: '/a', workspaceDependencies: ['b', 'c'] },
         b: { location: '/b', workspaceDependencies: ['d'] },
         c: { location: '/c', workspaceDependencies: undefined },
         d: { location: '/d', workspaceDependencies: ['a'] },
       };
-      expect(flattenWorkspaceDependencies(workspaces).get('a')).toEqual(
-        new Set(['a', 'b', 'c', 'd']),
-      );
+      expect(() => walkWorkspaceDependencies(workspaces, 'a')).toThrow();
+    });
+
+    it('throws on cycles in later descendants', () => {
+      const workspaces: Record<string, LiteWorkSpaceRecord> = {
+        a: { location: '/a', workspaceDependencies: ['b'] },
+        b: { location: '/b', workspaceDependencies: ['c'] },
+        c: { location: '/c', workspaceDependencies: ['b'] },
+      };
+      expect(() => walkWorkspaceDependencies(workspaces, 'a')).toThrow();
     });
   });
 });
--- packages/dependency-resolver/src/index.ts
+++ packages/dependency-resolver/src/index.ts
@@ -4,50 +4,68 @@ export interface LiteWorkSpaceRecord {
   version?: string;
 }
 
-// interface WorkspaceDependencyRecord {
-//   flatWorkspaceDependencies: Set<string>;
-//   hasCycle: boolean;
-// }
+type OrderedDependencies = Map<string, number>;
+type OrderedUnvisited = { name: string; level: number };
 
-// type WorkspaceDependencies = Map<string, WorkspaceDependencyRecord>;
-
-export function flattenWorkspaceDependencies(
+export function walkWorkspaceDependencies(
   workspaces: Record<string, LiteWorkSpaceRecord>,
-): Map<string, Set<string>> {
-  const cache: Map<string, Set<string>> = new Map();
+  workspaceName: string,
+): OrderedDependencies {
+  // Initialize the unvisited list with the immediate dependency array.
+  const unvisited: OrderedUnvisited[] = (
+    workspaces[workspaceName].workspaceDependencies ?? []
+  ).map((dep) => ({ name: dep, level: 1 }));
+  // visited holds all the nodes that we've visited previously
+  const visited: OrderedDependencies = new Map();
+  // cycleBreaker holds our DFS path and helps identifying cycles
+  const cycleBreaker: Set<string> = new Set();
 
-  Object.keys(workspaces).forEach((workspaceName) => {
-    if (!cache.has(workspaceName)) {
-      cacheDependencies(workspaceName);
+  while (unvisited.length) {
+    // Consume the remaining unvisited descendants one by one
+    const unvisitedDependency = unvisited.shift();
+    if (!unvisitedDependency) continue;
+
+    const { name: currentDependencyName, level: currentDependencyDepth } =
+      unvisitedDependency;
+
+    // If we're re-visiting a descendant in the current DFS run we have a cycle
+    if (cycleBreaker.has(currentDependencyName)) {
+      // Sets are guaranteed to be iterable in insertion order, so they will represent our traversal order faithfully
+      // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set
+      throw new Error(`Cycle detected: ${[...cycleBreaker].join(' -> ')}`);
     }
-  });
+    cycleBreaker.add(currentDependencyName);
 
-  return cache;
-
-  function cacheDependencies(
-    packageName: string,
-    done: Set<string> = new Set(),
-  ): Set<string> {
-    done.add(packageName);
+    // Get the next immediate dependencies of the dependency we're visiting.
     const immediateDependencies =
-      workspaces[packageName].workspaceDependencies || [];
-
-    let recursiveDeps: string[] = [];
-    for (const dependency of immediateDependencies) {
-      const fromCache = cache.get(dependency);
-      if (fromCache) {
-        recursiveDeps = [...fromCache];
-        continue;
-      } else if (!done.has(dependency)) {
-        recursiveDeps = [
-          ...recursiveDeps,
-          ...cacheDependencies(dependency, done),
-        ];
-      }
+      workspaces[currentDependencyName].workspaceDependencies;
+    // If we got to an end node, we finish the current DFS traversal: reset the cycle breaker
+    if (!immediateDependencies || !immediateDependencies.length) {
+      cycleBreaker.clear();
     }
 
-    const result = new Set([...immediateDependencies, ...recursiveDeps]);
-    cache.set(packageName, result);
-    return result;
+    // Add current dependency to the visited set.
+    // If we already visited it at a lower depth in the graph, raise its level to the current depth
+    // (this dependency could be a dependency of some other node, but since is also a dependency of *this* node, it gets the bigger depth of the two)
+    const dependencyLevel = visited.has(currentDependencyName)
+      ? Math.max(
+          currentDependencyDepth,
+          visited.get(currentDependencyName) ?? -1,
+        )
+      : currentDependencyDepth;
+    visited.set(currentDependencyName, dependencyLevel);
+
+    // All our immediate dependencies are inserted into unvisited, with a depth level = this node + 1
+    if (immediateDependencies) {
+      const levelledMaps = immediateDependencies?.map((dep) => ({
+        name: dep,
+        level: currentDependencyDepth + 1,
+      }));
+      // If we insert the immediate dependencies at the end (push), we do a BFS walk.
+      // If we insert them at the star (unshift), we do a DFS walk. We want to go DFS because it's easier to detect cycles.
+      unvisited.unshift(...levelledMaps);
+    }
   }
+
+  return visited;
 }
