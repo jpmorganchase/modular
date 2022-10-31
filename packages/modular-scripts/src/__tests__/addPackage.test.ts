0c72bcdefc29ee4a6150b9fba8af06b3debffc42
Alberto Brusa <alberto.brusa@live.com>
2022-10-31 14:24:58 +0000

    Refactor addPackage tests to not run yarn and use a separate temp testing space Added Templates tests

--- packages/modular-scripts/src/__tests__/addPackage.test.ts
+++ packages/modular-scripts/src/__tests__/addPackage.test.ts
@@ -7,47 +7,43 @@ import getModularRoot from '../utils/getModularRoot';
 
 import type { CoreProperties } from '@schemastore/package';
 import getAllFiles from '../utils/getAllFiles';
+import { createModularTestContext } from '../test/utils';
 
 const modularRoot = getModularRoot();
-const tempTemplatePath = path.join(modularRoot, 'packages', 'template');
-const tempPackagePath = path.join(modularRoot, 'packages', 'package');
 
-// These tests must be executed sequentially with `--runInBand`.
-
-const packagesPath = path.join(getModularRoot(), 'packages');
 const templatesPath = path.join(modularRoot, '__fixtures__', 'templates');
+
+// Copy app template needed for tests to fixtures
+fs.copySync(
+  path.join(modularRoot, 'packages', 'modular-template-app'),
+  path.join(templatesPath, 'modular-template-app'),
+);
+
+const tempModularRepo = createModularTestContext(templatesPath);
+const packagesPath = path.join(tempModularRepo, 'packages');
+
+/**
+ * Reset 'packages' workspace in temporary Modular Repo
+ */
+function cleanup() {
+  rimraf.sync(packagesPath);
+  fs.mkdirSync(packagesPath);
+}
 
 function modular(str: string, opts: Record<string, unknown> = {}) {
   return execa('yarnpkg', ['modular', ...str.split(' ')], {
-    cwd: modularRoot,
+    cwd: tempModularRepo,
     cleanup: true,
+    stdio: 'inherit',
     ...opts,
   });
 }
-
-function cleanup() {
-  rimraf.sync(path.join(packagesPath, 'sample-app'));
-  rimraf.sync(path.join(packagesPath, 'nested'));
-  rimraf.sync(tempTemplatePath);
-  rimraf.sync(tempPackagePath);
-
-  // run yarn so yarn.lock gets reset
-  return execa.sync('yarnpkg', ['--silent'], {
-    cwd: modularRoot,
-  });
-}
-
-beforeAll(cleanup);
-afterAll(cleanup);
 
 describe('When setting a base directory for an app', () => {
   it('fails if trying to add an app outside the "workspaces" directories', async () => {
     await expect(
       modular(
         'add @scoped/will-not-create-app --path some/other/basepath --unstable-type app',
-        {
-          stdio: 'inherit',
-        },
       ),
     ).rejects.toThrow();
   });
@@ -55,25 +51,21 @@ describe('When setting a base directory for an app', () => {
 
 describe('When working with a scoped app', () => {
   beforeAll(async () => {
-    await modular('add @scoped/sample-app --unstable-type app', {
-      stdio: 'inherit',
-    });
+    await modular('add @scoped/sample-app --unstable-type app');
   });
 
   afterAll(cleanup);
 
   it('creates the app in the expected directory, with the expected name', async () => {
     const manifest = (await fs.readJSON(
-      path.join(modularRoot, 'packages', 'sample-app', 'package.json'),
+      path.join(packagesPath, 'sample-app', 'package.json'),
     )) as CoreProperties;
     expect(manifest.name).toEqual('@scoped/sample-app');
   });
 
   it('fails if trying to add another app with the same name', async () => {
     await expect(
-      modular('add @scoped/sample-app --unstable-type app', {
-        stdio: 'inherit',
-      }),
+      modular('add @scoped/sample-app --unstable-type app'),
     ).rejects.toThrow();
   });
 
@@ -81,18 +73,13 @@ describe('When working with a scoped app', () => {
     await expect(
       modular(
         'add @scoped/sample-app --unstable-type app --path packages/wont/happen',
-        {
-          stdio: 'inherit',
-        },
       ),
     ).rejects.toThrow();
   });
 
   it('fails trying to add another app in the same path (as scope is discarded)', async () => {
     await expect(
-      modular('add sample-app --unstable-type app', {
-        stdio: 'inherit',
-      }),
+      modular('add sample-app --unstable-type app'),
     ).rejects.toThrow();
   });
 });
@@ -101,9 +88,6 @@ describe('When working with an app installed in a custom directory', () => {
   beforeAll(async () => {
     await modular(
       'add @scoped/sample-app --unstable-type app --path packages/nested/scoped',
-      {
-        stdio: 'inherit',
-      },
     );
   });
 
@@ -111,23 +95,14 @@ describe('When working with an app installed in a custom directory', () => {
 
   it('creates the app in the custom directory, with the expected name', async () => {
     const manifest = (await fs.readJSON(
-      path.join(
-        modularRoot,
-        'packages',
-        'nested',
-        'scoped',
-        'sample-app',
-        'package.json',
-      ),
+      path.join(packagesPath, 'nested', 'scoped', 'sample-app', 'package.json'),
     )) as CoreProperties;
     expect(manifest.name).toEqual('@scoped/sample-app');
   });
 
   it('fails if trying to add another app with the same name in the default path', async () => {
     await expect(
-      modular('add @scoped/sample-app --unstable-type app', {
-        stdio: 'inherit',
-      }),
+      modular('add @scoped/sample-app --unstable-type app'),
     ).rejects.toThrow();
   });
 
@@ -135,114 +110,89 @@ describe('When working with an app installed in a custom directory', () => {
     await expect(
       modular(
         'add sample-app --unstable-type app --path packages/nested/scoped',
-        {
-          stdio: 'inherit',
-        },
       ),
     ).rejects.toThrow();
   });
+});
 
-  describe('When adding a module from a template without files filter', () => {
-    beforeAll(async () => {
-      // Copy into the workspace the template needed for the test
-      await fs.copy(
-        path.join(templatesPath, 'modular-template-no-filter'),
-        path.join(tempTemplatePath),
-        { overwrite: true },
-      );
-
-      // Run yarnpkg to update workspace so that it picks up the template
-      await execa('yarnpkg', { cwd: modularRoot, cleanup: true });
-
-      await execa(
-        'yarnpkg',
-        ['modular', 'add', 'package', '--template', 'no-filter'],
-        { cwd: modularRoot, cleanup: true },
-      );
-    });
-
-    afterAll(cleanup);
-
-    it('generates the package.json', async () => {
-      const manifest = (await fs.readJSON(
-        path.join(tempPackagePath, 'package.json'),
-      )) as CoreProperties;
-      expect(JSON.stringify(manifest, null, 2)).toMatchInlineSnapshot(`
-        "{
-          \\"name\\": \\"package\\",
-          \\"private\\": true,
-          \\"modular\\": {
-            \\"type\\": \\"app\\"
-          },
-          \\"version\\": \\"1.0.0\\"
-        }"
-      `);
-    });
-
-    it('copies all files declared in the template', () => {
-      let files = getAllFiles(path.join(tempPackagePath));
-      files = files.map((file) => file.substring(file.lastIndexOf('/')));
-      expect(files).toMatchInlineSnapshot(`
-        Array [
-          "/CHANGELOG.md",
-          "/package.json",
-          "/robots.txt",
-          "/no-filter.test.ts",
-          "/index.tsx",
-          "/tsconfig.json",
-        ]
-      `);
-    });
+describe('When adding a module from a template without a files filter', () => {
+  const newModulePath = path.join(packagesPath, 'no-filter-module');
+  beforeAll(async () => {
+    await modular('add no-filter-module --template no-filter');
   });
 
-  describe('When adding a module from a template with files filter', () => {
-    beforeAll(async () => {
-      // Copy into the workspace the template needed for the test
-      await fs.copy(
-        path.join(templatesPath, 'modular-template-filter'),
-        path.join(tempTemplatePath),
-        { overwrite: true },
-      );
+  afterAll(cleanup);
 
-      // Run yarnpkg to update workspace so that it picks up the template
-      await execa('yarnpkg', { cwd: modularRoot, cleanup: true });
+  it('generates the package.json', async () => {
+    const manifest = (await fs.readJSON(
+      path.join(newModulePath, 'package.json'),
+    )) as CoreProperties;
 
-      await execa(
-        'yarnpkg',
-        ['modular', 'add', 'package', '--template', 'filter'],
-        { cwd: modularRoot, cleanup: true },
-      );
-    });
+    // Expect name to be as set by command, and type as set by template
+    expect(JSON.stringify(manifest, null, 2)).toMatchInlineSnapshot(`
+      "{
+        \\"name\\": \\"no-filter-module\\",
+        \\"private\\": true,
+        \\"modular\\": {
+          \\"type\\": \\"app\\"
+        },
+        \\"version\\": \\"1.0.0\\"
+      }"
+    `);
+  });
 
-    afterAll(cleanup);
+  it("copies all files in the template's package.json files field", () => {
+    let files = getAllFiles(newModulePath);
+    files = files.map((file) => file.substring(file.lastIndexOf('/')));
+    expect(files).toMatchInlineSnapshot(`
+      Array [
+        "/CHANGELOG.md",
+        "/package.json",
+        "/robots.txt",
+        "/no-filter.test.ts",
+        "/index.tsx",
+        "/tsconfig.json",
+      ]
+    `);
+  });
+});
 
-    it('generates the package.json', async () => {
-      const manifest = (await fs.readJSON(
-        path.join(tempPackagePath, 'package.json'),
-      )) as CoreProperties;
-      expect(JSON.stringify(manifest, null, 2)).toMatchInlineSnapshot(`
-        "{
-          \\"name\\": \\"package\\",
-          \\"private\\": true,
-          \\"modular\\": {
-            \\"type\\": \\"app\\"
-          },
-          \\"version\\": \\"1.0.0\\"
-        }"
-      `);
-    });
+describe('When adding a module from a template with a files filter', () => {
+  const newModulePath = path.join(packagesPath, 'filter-module');
+  beforeAll(async () => {
+    await modular('add filter-module --template filter');
+  });
 
-    it("copies all files declared in the template's package.json files field", () => {
-      let files = getAllFiles(path.join(tempPackagePath));
-      files = files.map((file) => file.substring(file.lastIndexOf('/')));
-      expect(files).toMatchInlineSnapshot(`
-        Array [
-          "/package.json",
-          "/no-filter.test.ts",
-          "/index.tsx",
-          "/tsconfig.json",
-        ]
-      `);
-    });
+  afterAll(cleanup);
+
+  it('generates the package.json', async () => {
+    const manifest = (await fs.readJSON(
+      path.join(newModulePath, 'package.json'),
+    )) as CoreProperties;
+
+    // Expect name to be as set by command, and type as set by template
+    expect(JSON.stringify(manifest, null, 2)).toMatchInlineSnapshot(`
+      "{
+        \\"name\\": \\"filter-module\\",
+        \\"private\\": true,
+        \\"modular\\": {
+          \\"type\\": \\"app\\"
+        },
+        \\"version\\": \\"1.0.0\\"
+      }"
+    `);
+  });
+
+  it("copies only files declared in the template's package.json files field, and the package.json itself", () => {
+    let files = getAllFiles(newModulePath);
+    files = files.map((file) => file.substring(file.lastIndexOf('/')));
+    expect(files).toMatchInlineSnapshot(`
+      Array [
+        "/package.json",
+        "/filter.test.ts",
+        "/index.tsx",
+        "/tsconfig.json",
+      ]
+    `);
   });
 });
--- packages/modular-scripts/src/test/utils.ts
+++ packages/modular-scripts/src/test/utils.ts
@@ -2,9 +2,11 @@ import _rimraf from 'rimraf';
 import execa from 'execa';
 import fs from 'fs-extra';
 import path from 'path';
+import * as tmp from 'tmp';
 import { promisify } from 'util';
 
 import getModularRoot from '../utils/getModularRoot';
+import type { ModularPackageJson } from '@modular-scripts/modular-types';
 
 const rimraf = promisify(_rimraf);
 
@@ -52,4 +54,63 @@ export async function addFixturePackage(
       packageSrcDir,
     );
   }
+}
+
+/**
+ * Creates a temporary Modular repo with the provided templates 'installed' and Modular node_modules symlinked
+ * @param templatesPath Path to fixture to copy in
+ * @returns Path to temporary directory
+ */
+export function createModularTestContext(templatesPath: string): string {
+  // Modular node_modules are copied in the parent folder
+  const tempDir = tmp.dirSync().name;
+  fs.symlinkSync(
+    path.join(modularRoot, 'node_modules'),
+    path.join(tempDir, 'node_modules'),
+  );
+
+  // Actual mock modular repo is nested underneath so that it can have it's own node_modules
+  const tempModularRepo = path.join(tempDir, 'temp_repo');
+  const tempTemplatesPath = path.join(tempModularRepo, 'templates');
+
+  // Copy in the templates
+  fs.copySync(templatesPath, tempTemplatesPath);
+
+  // Symlink the templates into node_modules
+  const tempRepoNodeModules = path.join(tempModularRepo, 'node_modules');
+  fs.mkdirSync(tempRepoNodeModules);
+  const templates = fs.readdirSync(tempTemplatesPath);
+  templates.forEach((template) =>
+    fs.symlinkSync(
+      path.join(tempTemplatesPath, template),
+      path.join(tempRepoNodeModules, template),
+    ),
+  );
+
+  // Create mock modular package.json
+  const packageJson: Partial<ModularPackageJson> = {
+    name: 'temp',
+    license: 'MIT',
+    private: true,
+    modular: {
+      type: 'root',
+    },
+    workspaces: ['packages/**', 'templates/**'],
+    scripts: {
+      modular: `ts-node ${modularRoot}/packages/modular-scripts/src/cli.ts`,
+    },
+    browserslist: {
+      production: ['>0.2%', 'not dead', 'not op_mini all'],
+      development: [
+        'last 1 chrome version',
+        'last 1 firefox version',
+        'last 1 safari version',
+      ],
+    },
+  };
+
+  fs.writeJSONSync(path.join(tempModularRepo, 'package.json'), packageJson, {
+    spaces: 2,
+  });
+  return tempModularRepo;
 }
