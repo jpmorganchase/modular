4a541a904ebff5d4a56a16a3bbb6c57669c17fac
Cristiano Belloni <cristiano-belloni@users.noreply.github.com>
2022-12-01 15:46:21 +0000

    Include all workspace files in webpack / rollup build configurations (#2219)
    * Construct the include directories from all workspaces
    
    * Support Windows paths
    
    * Create tough-mugs-dream.md
    
    * Add tests and fixtures
    
    * Fix packages
    
    * Fix changeset

---
+++ __fixtures__/custom-workspace-root/apps/app/src/index.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+import App from './App';
+import './index.css';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root'),
+);
