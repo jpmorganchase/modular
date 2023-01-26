0ed78f5c84ee1aefd54b25160c1f916702f52cc6
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2023-01-26 11:09:00 +0000

    Make snapshots deterministic

--- packages/modular-scripts/src/__tests__/utils/getWorkspaceInfo.test.ts
+++ packages/modular-scripts/src/__tests__/utils/getWorkspaceInfo.test.ts
@@ -2,7 +2,11 @@ import { getWorkspaceInfo } from '../../utils/getWorkspaceInfo';
 
 test('getWorkspaceInfo', async () => {
   const workspace = await getWorkspaceInfo();
-  Object.values(workspace).forEach((pkg) => {
-    expect(pkg).toMatchSnapshot({ version: expect.any(String) as unknown });
-  });
+  Object.entries(workspace)
+    .sort(([packageA], [packageB]) =>
+      packageA < packageB ? -1 : packageA > packageB ? 1 : 0,
+    )
+    .forEach(([, pkg]) => {
+      expect(pkg).toMatchSnapshot({ version: expect.any(String) as unknown });
+    });
 });
