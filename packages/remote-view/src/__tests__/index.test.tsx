9253043ea6c071296ebdb53c459b58cd9ab94ba1
Sam Brown <sam.brown@jpmorgan.com>
2023-02-17 14:48:55 +0000

    Passing test against a direct esm view

--- packages/remote-view/src/__tests__/index.test.tsx
+++ packages/remote-view/src/__tests__/index.test.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
-import path from 'path';
-import { spawn } from 'child_process';
-import { render, screen } from '@testing-library/react';
+/* eslint-disable testing-library/prefer-screen-queries */
 
-import { RemoteViewExample } from './RemoteViewExample';
+import path from 'path';
+import puppeteer from 'puppeteer';
+import { getDocument, queries, waitFor } from 'pptr-testing-library';
+import { spawn } from 'child_process';
+
+// import { RemoteViewExample } from './RemoteViewExample';
 
 const controller = new AbortController();
 
@@ -26,28 +28,64 @@ function bootFakeEsmCdn() {
 }
 
 describe('remote-view', () => {
+  let browser: puppeteer.Browser;
+
   beforeAll(async () => {
     await bootFakeEsmCdn();
+
+    const launchArgs: puppeteer.LaunchOptions &
+      puppeteer.BrowserLaunchArgumentOptions = {
+      // always run in headless - if you want to debug this locally use the env var to
+      headless: !Boolean(process.env.NO_HEADLESS_TESTS),
+      args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    };
+
+    browser = await puppeteer.launch(launchArgs);
   });
 
-  afterAll(() => {
+  afterAll(async () => {
     // Terminate the fake ESM CDN after tests have run
     controller.abort();
+
+    // Terminate the puppeteer browser
+    if (browser) {
+      await browser.close();
+    }
   });
 
-  // TODO
-  // Render some <RemoteView />s
-  // The remote views should load (at least 2) and render
-  // The iframe fallback would work
-
   it('should work', async () => {
-    // const content = await fetch(
-    //   'http://localhost:8484/esm-view-card/package.json',
-    // );
-    // console.log('fetched this content', content);
-    expect(1 + 1).toBe(2);
+    // TODO update this to render an actual <RemoteViewExample />
 
-    render(<RemoteViewExample />);
-    await screen.findByText('My List');
+    const page = await browser.newPage();
+    await page.goto('http://localhost:8484/esm-view-card/index.html', {});
+
+    // 1. The card should render with it's default content
+    const $document = await getDocument(page);
+    // eslint-disable-next-line @typescript-eslint/no-misused-promises
+    await waitFor(() => queries.findByText($document, 'Some card contents'));
+
+    const $button = await queries.findByText($document, 'Change card content');
+    // eslint-disable-next-line jest-dom/prefer-in-document
+    expect($button).toBeTruthy();
+
+    // 2. Clicking the button should work
+    const buttonSelector = 'button';
+    await page.waitForSelector(buttonSelector);
+    await page.click(buttonSelector);
+
+    const $updated = await queries.findByText(
+      $document,
+      'Some mutated card contents',
+    );
+    // eslint-disable-next-line jest-dom/prefer-in-document
+    expect($updated).toBeTruthy();
+
+    // 3. React & ReactDOM still work, the content of the remote view got updated
+    const contentSelector = 'span';
+    const updatedContentSelector = await page.waitForSelector(contentSelector);
+    const updatedContent = await updatedContentSelector?.evaluate(
+      (el) => el.textContent,
+    );
+    expect(updatedContent).toBe('Some mutated card contents');
   });
 });
