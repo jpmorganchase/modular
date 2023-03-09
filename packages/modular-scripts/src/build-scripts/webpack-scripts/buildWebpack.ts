d650f488f139af77a58b3760ddf2e11425e869b0
Alberto Brusa <alberto.brusa@live.com>
2023-03-09 09:36:37 +0000

    Cleanup

--- packages/modular-scripts/src/build-scripts/webpack-scripts/buildWebpack.ts
+++ packages/modular-scripts/src/build-scripts/webpack-scripts/buildWebpack.ts
@@ -21,13 +21,6 @@ export default async function buildWebpack(
   styleImports: Set<string>,
   paths: Paths,
 ): Promise<StatsCompilation> {
-  // Makes the script crash on unhandled rejections instead of silently
-  // ignoring them. In the future, promise rejections that are not handled will
-  // terminate the Node.js process with a non-zero exit code.
-  process.on('unhandledRejection', (err) => {
-    throw err;
-  });
-
   const webpackConfig: WebpackConfiguration = await getConfig(
     true,
     esbuildTargetFactory,
