45145d3a8d6c8e1d78a79d77ebd90a2369513208
Sam Brown <sam.brown@jpmorgan.com>
2023-03-16 16:50:08 +0000

    Add test that covers https in esbuild mode

---
+++ packages/modular-scripts/src/__tests__/start.test.ts
@@ -0,0 +1,92 @@
+/**
+ * @jest-environment jsdom
+ */
+
+/* eslint-disable testing-library/prefer-screen-queries */
+
+import path from 'path';
+import fs from 'fs-extra';
+import { getDocument, queries, waitFor } from 'pptr-testing-library';
+import puppeteer from 'puppeteer';
+
+import { startApp, DevServer } from './start-app';
+import { createModularTestContext, runModularForTests } from '../test/utils';
+
+// Temporary text context paths
+let tempModularRepo: string;
+
+// These tests must be executed sequentially with `--runInBand`.
+const targetedView = 'sample-app';
+
+describe('modular start', () => {
+  beforeAll(async () => {
+    tempModularRepo = createModularTestContext();
+    const tempPackagesPath = path.join(tempModularRepo, 'packages');
+    runModularForTests(
+      tempModularRepo,
+      'add sample-app --unstable-type app --unstable-name sample-app',
+    );
+
+    await fs.copyFile(
+      path.join(__dirname, 'TestApp.test-tsx'),
+      path.join(tempPackagesPath, targetedView, 'src', 'App.tsx'),
+    );
+  });
+
+  describe('when starting an app in esbuild mode', () => {
+    let browser: puppeteer.Browser;
+    let devServer: DevServer;
+    let port: string;
+
+    beforeAll(async () => {
+      const launchArgs: puppeteer.LaunchOptions &
+        puppeteer.BrowserLaunchArgumentOptions = {
+        // always run in headless - if you want to debug this locally use the env var to
+        headless: !Boolean(process.env.NO_HEADLESS_TESTS),
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox',
+          '--allow-insecure-localhost',
+        ],
+      };
+
+      browser = await puppeteer.launch(launchArgs);
+      port = '4000';
+      devServer = await startApp(
+        targetedView,
+        {
+          env: { PORT: port, USE_MODULAR_ESBUILD: 'true', HTTPS: 'true' },
+        },
+        tempModularRepo,
+      );
+    });
+
+    afterAll(async () => {
+      if (browser) {
+        await browser.close();
+      }
+      if (devServer) {
+        // this is the problematic bit, it leaves hanging node processes
+        // despite closing the parent process. Only happens in tests!
+        void devServer.kill();
+      }
+    });
+
+    it('successfully starts up using https', async () => {
+      const page = await browser.newPage();
+      await page.goto(`https://localhost:${port}`, {});
+
+      const $document = await getDocument(page);
+
+      // eslint-disable-next-line @typescript-eslint/no-misused-promises
+      await waitFor(() => queries.findByTestId($document, 'test-this'));
+
+      const $text = await queries.findByText(
+        $document,
+        'this is a modular app',
+      );
+      // eslint-disable-next-line jest-dom/prefer-in-document
+      expect($text).toBeTruthy();
+    });
+  });
+});
