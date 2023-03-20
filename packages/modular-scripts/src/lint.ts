02663b88b92c32a65d3fe29c16b418126518749b
Cristiano Belloni <cristiano.belloni@jpmorgan.com>
2023-03-20 18:11:03 +0000

    selective and --staged are exclusive, regex are additive

--- packages/modular-scripts/src/lint.ts
+++ packages/modular-scripts/src/lint.ts
@@ -10,7 +10,6 @@ import { addFiles, getDiffedFiles, getStagedFiles } from './utils/gitActions';
 import * as logger from './utils/logger';
 import { generateJestConfig } from './test/utils';
 import { getAllWorkspaces } from './utils/getAllWorkspaces';
-import { WorkspaceContent } from '@modular-scripts/modular-types';
 export interface LintOptions {
   all: boolean;
   fix: boolean;
@@ -25,50 +24,77 @@ async function lint(
   const { all = false, fix = false, staged = false, packages } = options;
   const modularRoot = getModularRoot();
   const lintExtensions = ['.ts', '.tsx', '.js', '.jsx'];
-  let targetedFiles = ['<rootDir>/**/src/**/*.{js,jsx,ts,tsx}'];
+  let runnerMatch = ['<rootDir>/**/src/**/*.{js,jsx,ts,tsx}'];
 
   console.log({ all, fix, staged, packages, regexes });
-  const workspaces = await getAllWorkspaces();
+  const [packageMap] = await getAllWorkspaces();
 
-  // --packages alone means "only the diffed files contained in these packages"
-  // --packages + --staged means "only the staged files contained in these packages"
-  // --packages + --all means "only the files contained in these packages"
-  const filterPackages = packages?.length
-    ? (p: string) => isPathInPackageList(p, packages, workspaces)
-    : () => true;
+  const selectiveOptionSpecified = packages?.length; // || ancestors etc.
 
-  if (!all && (!isCI || staged) && regexes.length === 0) {
-    const diffedFiles = staged ? getStagedFiles() : getDiffedFiles();
-
-    console.log({ diffedFiles });
-
-    if (diffedFiles.length === 0) {
-      logger.log(
-        'No diffed files detected. Use the `--all` option to lint the entire codebase',
-      );
-      return;
-    }
-    const targetExts = diffedFiles
-      .filter(filterPackages)
-      .filter((p: string) => lintExtensions.includes(path.extname(p)))
-      .map((p: string) => `<rootDir>/${p}`);
-
-    // if none of the diffed files meet the extension criteria, do not lint
-    // end the process early with a success
-    if (!targetExts.length) {
-      logger.debug('No diffed js,jsx,ts,tsx files found');
-      return;
-    }
-    targetedFiles = targetExts;
+  if (all && selectiveOptionSpecified) {
+    logger.warn(
+      'You specified --all already; selective options are redundant.',
+    );
+  }
+  if (staged && selectiveOptionSpecified) {
+    logger.error("Can't specify --staged along with selective options");
+    process.exit(-1);
   }
 
-  console.log({ targetedFiles });
+  if (!all && !isCI) {
+    if (selectiveOptionSpecified) {
+      // TODO: Calculate packages from selective options
+
+      // Calculate the match regexes for the selective options
+      const selectiveMatch = packages
+        .map((packageName) => {
+          const packageLocation = packageMap.get(packageName)?.location;
+          return packageLocation
+            ? `<rootDir>/${packageLocation}/src/**/*.{js,jsx,ts,tsx}`
+            : undefined;
+        })
+        .filter(Boolean) as string[];
+
+      // If there are no selective matches and no regexes are specified, there is nothing to test
+      if (!selectiveMatch.length && !regexes.length) {
+        logger.warn('No target files to lint with the provided selection');
+        return;
+      }
+      // Narrow the matches to the selection matches
+      runnerMatch = selectiveMatch;
+    } else {
+      // Not selective and not --all; calculate the file regexes of --diff or --staged
+      if (staged && regexes.length === 0) {
+        const diffedFiles = staged ? getStagedFiles() : getDiffedFiles();
+
+        console.log({ diffedFiles });
+
+        if (diffedFiles.length === 0) {
+          logger.log(
+            'No diffed files detected. Use the `--all` option to lint the entire codebase',
+          );
+          return;
+        }
+        const targetExts = diffedFiles
+          .filter((p: string) => lintExtensions.includes(path.extname(p)))
+          .map((p: string) => `<rootDir>/${p}`);
+
+        // if none of the diffed files meet the extension criteria, do not lint
+        // end the process early with a success
+        if (!targetExts.length) {
+          logger.warn('No diffed target files to lint found');
+          return;
+        }
+        runnerMatch = targetExts;
+      }
+    }
+  }
 
   const jestEslintConfig = {
     runner: require.resolve('modular-scripts/jest-runner-eslint'),
     displayName: 'lint',
     rootDir: modularRoot,
-    testMatch: targetedFiles,
+    testMatch: runnerMatch,
     testPathIgnorePatterns: ['/node_modules/', '/dist/'],
   };
 
@@ -94,23 +120,14 @@ async function lint(
     });
 
     if (staged && fix) {
-      targetedFiles = targetedFiles.map((p) => p.replace('<rootDir>/', ''));
-      addFiles(targetedFiles);
+      runnerMatch = runnerMatch.map((p) => p.replace('<rootDir>/', ''));
+      addFiles(runnerMatch);
     }
   } catch (err) {
     logger.debug((err as ExecaError).message);
     // ✕ Modular lint did not pass
     throw new Error('\u2715 Modular lint did not pass');
   }
-}
-
-function isPathInPackageList(
-  p: string,
-  packages: string[],
-  workspaces: WorkspaceContent,
-) {
-  console.log('testing', { p, packages });
-  return false;
 }
 
 export default actionPreflightCheck(lint);
