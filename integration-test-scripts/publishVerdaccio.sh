f48c9143cf7fed61ff2553830f7c43aff40677ad
Alberto Brusa <alberto.brusa@live.com>
2023-03-30 12:46:18 +0100

    Try to fix publish by writing standalones script

---
+++ integration-test-scripts/publishVerdaccio.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+PACKAGES=("modular-scripts" "create-modular-react-app" "eslint-config-modular-app" "modular-template-app" "modular-template-esm-view" "modular-template-package" "modular-template-source" "modular-template-view")
+for package in $PACKAGES; do 
+    cd ./packages/$package
+    yarn publish --no-git-tag-version --no-commit-hooks --registry http://localhost:4873/ --new-version 99999.0.0
+    cd ../../
+done
