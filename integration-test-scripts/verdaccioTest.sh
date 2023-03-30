d8c20bc765bb112894de3d74d34b05e285cb6ff1
Alberto Brusa <alberto.brusa@live.com>
2023-03-30 14:02:17 +0100

    add localhost to yarn 3  allowlist

--- integration-test-scripts/verdaccioTest.sh
+++ integration-test-scripts/verdaccioTest.sh
@@ -16,7 +16,9 @@ if [ $YARN_VERSION -eq '1' ]
 then
     yarn config set registry http://localhost:4873/
 else
+    # Not sure why we have to set it again but it won't use it otherwise 
     yarn set version 3.5.0
+    yarn config set unsafeHttpWhitelist localhost
     yarn config set npmRegistryServer http://localhost:4873/
 fi
 
