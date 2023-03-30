d7393ce5e37c28c93bf6cf933d784875be4ef413
Alberto Brusa <alberto.brusa@live.com>
2023-03-30 12:00:38 +0100

    Implement integration tests

---
+++ integration-test-scripts/verdaccioTest.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+
+YARN_VERSION=$(yarn -v | cut -d "." -f1)
+# All templates to test except app which is already installed
+TEMPLATES=[app, esm-view, package, view, source]
+
+cd /tmp
+
+# Set registry with yarn version specific command
+if [ $YARN_VERSION -eq '1']
+then
+    yarn config set registry http://localhost:4873/
+else
+    yarn config set npmRegistryServer http://localhost:4873/
+fi
+
+yarn create modular-react-app test-repo --empty
+
+cd ./test-repo
+
+for template in $TEMPLATES; do 
+    yarn modular add $template --template $template
+    yarn modular test $template
+    yarn modular build $template
+    USE_MODULAR_ESBUILD=true yarn modular build $template
+    yarn modular analyze $template
+done
+
+yarn modular workspace
+yarn modular lint $TEMPLATES
+yarn modular typecheck
+
