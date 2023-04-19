ccffbfd909d4451c030be16b26adf53d3581d52a
Cristiano Belloni <cristiano-belloni@users.noreply.github.com>
2023-04-19 14:42:41 +0100

    Enable "strict" compiler option (#2352)
    * Enable strict compiler option
    
    * Create plenty-hounds-judge.md
    
    * Add docs
    
    * Adjust for falsy config values

---
+++ .changeset/plenty-hounds-judge.md
@@ -0,0 +1,5 @@
+---
+"modular-scripts": minor
+---
+
+Enable "strict" compiler option in the [typecheck allowlist](https://modular.js.org/commands/typecheck/#configuration)
--- packages/modular-scripts/src/typecheck.ts
+++ packages/modular-scripts/src/typecheck.ts
@@ -8,7 +8,10 @@ import actionPreflightCheck from './utils/actionPreflightCheck';
 import { getAllWorkspaces } from './utils/getAllWorkspaces';
 import { selectWorkspaces } from './utils/selectWorkspaces';
 
-import type { JSONSchemaForTheTypeScriptCompilerSConfigurationFile as TSConfig } from '@schemastore/tsconfig';
+import type {
+  JSONSchemaForTheTypeScriptCompilerSConfigurationFile as TSConfig,
+  CompilerOptionsDefinition,
+} from '@schemastore/tsconfig';
 
 type CompilerOptions = TSConfig['compilerOptions'];
 
@@ -19,7 +22,10 @@ export interface TypecheckOptions {
   compareBranch: string;
 }
 
-const COMPILER_OPTIONS_ALLOW_LIST: string[] = ['jsx'];
+const COMPILER_OPTIONS_ALLOW_LIST: (keyof CompilerOptionsDefinition)[] = [
+  'jsx',
+  'strict',
+];
 const DEFAULT_COMPILER_OPTIONS: CompilerOptions = {
   noEmit: true,
 };
@@ -55,7 +61,7 @@ function restrictUserTsconfig(
 
   // Where the user has defined allowlist-supported compiler options, apply them
   for (const item of COMPILER_OPTIONS_ALLOW_LIST) {
-    if (compilerOptions && compilerOptions[item]) {
+    if (compilerOptions?.[item] !== undefined) {
       // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
       combinedCompilerOptions[item] = compilerOptions[item];
     }
